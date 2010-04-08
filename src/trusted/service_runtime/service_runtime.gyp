sbc100/native_client

commit 991b5305295823f2b7b42d88411915464473ca67

Refactor: Unify sel_validate_image.c variants into one file
This provides NaClValidateCode() as a uniform interface to all the
validators.  This will be used by the dynamic code loading syscall.

This also reduces code duplication a little.  The
NaClIgnoreValidatorResult() logic is no longer duplicated.

Add check for return value of NaClValidatorStateCreate() that was
previously missing.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=318

Review URL: http://codereview.chromium.org/1585004

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1969 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/service_runtime.gyp
@@ -80,6 +80,7 @@
           'sel_mem.c',
           'sel_util.c',
           'sel_util-inl.c',
+          'sel_validate_image.c',
           'web_worker_stub.c',
         ],
         'sources!': [
@@ -138,7 +139,6 @@
                 'arch/arm/nacl_tls.c',
                 'arch/arm/sel_ldr_arm.c',
                 'arch/arm/sel_addrspace_arm.c',
-                'arch/arm/sel_validate_image.c',
                 'arch/arm/nacl_switch.S',
                 'arch/arm/nacl_syscall.S',
                 'arch/arm/springboard.S',
@@ -263,6 +263,7 @@
             '<(DEPTH)/native_client/src/trusted/desc/desc.gyp:nrd_xfer64',
             '<(DEPTH)/native_client/src/shared/gio/gio.gyp:gio64',
             '<(DEPTH)/native_client/src/trusted/validator_x86/validator_x86.gyp:ncvalidate_sfi64',
+            '<(DEPTH)/native_client/src/trusted/validator_x86/validator_x86.gyp:ncopcode_utils_gen',
             '<(DEPTH)/native_client/src/shared/srpc/srpc.gyp:nonnacl_srpc64',
             'arch/x86/service_runtime_x86.gyp:service_runtime_x86_common64',
             'arch/x86_64/service_runtime_x86_64.gyp:service_runtime_x86_64',

src/trusted/service_runtime/arch/arm/sel_validate_image.c
@@ -1,48 +1,0 @@
-/*
- * Copyright 2009 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
-
-/*
- * Invoke validator for NaCl secure ELF loader (NaCl SEL).
- */
-
-#include "native_client/src/shared/platform/nacl_log.h"
-#include "native_client/src/trusted/service_runtime/sel_ldr.h"
-#include "native_client/src/trusted/validator_arm/v2/ncvalidate.h"
-
-static int g_ignore_validator_result = 0;
-void NaClIgnoreValidatorResult() {
-  g_ignore_validator_result = 1;
-}
-
-NaClErrorCode NaClValidateImage(struct NaClApp  *nap) {
-  uintptr_t               memp;
-  uintptr_t               endp;
-  size_t                  regionsize;
-
-  memp = nap->mem_start + NACL_TRAMPOLINE_END;
-  endp = nap->mem_start + nap->static_text_end;
-  regionsize = endp - memp;
-  if (endp < memp) {
-    return LOAD_NO_MEMORY;
-  }
-
-  if (NCValidateSegment((uint8_t *)memp, memp, regionsize) == 0) {
-    return LOAD_OK;
-  } else {
-    if (g_ignore_validator_result) {
-      NaClLog(LOG_ERROR, "VALIDATION FAILED: continuing anyway...\n");
-      return LOAD_OK;
-    } else {
-      NaClLog(LOG_ERROR, "VALIDATION FAILED.\n");
-      NaClLog(LOG_ERROR,
-              "Run sel_ldr in debug mode to ignore validation failure.\n");
-      NaClLog(LOG_ERROR,
-              "Run ncval <module-name> for validation error details.\n");
-      return LOAD_VALIDATION_FAILED;
-    }
-  }
-}
-

src/trusted/service_runtime/arch/x86/sel_validate_image.c
@@ -1,75 +1,0 @@
-/*
- * Copyright 2008 The Native Client Authors.  All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
-
-/*
- * Invoke validator for NaCl secure ELF loader (NaCl SEL).
- */
-
-#include <string.h>
-
-#include "native_client/src/shared/platform/nacl_log.h"
-#include "native_client/src/trusted/service_runtime/sel_ldr.h"
-#include "native_client/src/trusted/validator_x86/ncvalidate.h"
-#include "native_client/src/trusted/validator_x86/ncvalidate_iter.h"
-
-static int g_ignore_validator_result = 0;
-void NaClIgnoreValidatorResult() {
-  g_ignore_validator_result = 1;
-}
-
-NaClErrorCode NaClValidateImage(struct NaClApp  *nap) {
-  uintptr_t               memp;
-  uintptr_t               endp;
-#if NACL_TARGET_SUBARCH == 32
-  struct NCValidatorState *vstate;
-#else
-  struct NaClValidatorState *vstate;
-#endif
-  size_t                  regionsize;
-  NaClErrorCode           rcode = LOAD_BAD_FILE;
-
-  memp = nap->mem_start + NACL_TRAMPOLINE_END;
-  endp = nap->mem_start + nap->static_text_end;
-  regionsize = endp - memp;
-  if (endp < memp) {
-    return LOAD_NO_MEMORY;
-  }
-#if NACL_TARGET_SUBARCH == 32
-  vstate = NCValidateInit(memp, endp, nap->bundle_size);
-  if (NULL == vstate) return LOAD_BAD_FILE;
-  NCValidateSegment((uint8_t *)memp, memp, regionsize, vstate);
-  if (NCValidateFinish(vstate) == 0) {
-    rcode = LOAD_OK;
-  }
-  NCValidateFreeState(&vstate);
-#else
-  vstate = NaClValidatorStateCreate(memp,
-                                    regionsize,
-                                    nap->bundle_size,
-                                    RegR15,
-                                    1,
-                                    stderr);
-  NaClValidateSegment((uint8_t*)memp, memp, regionsize, vstate);
-  if (NaClValidatesOk(vstate)) {
-    rcode = LOAD_OK;
-  }
-  NaClValidatorStateDestroy(vstate);
-#endif
-  if (LOAD_OK != rcode) {
-    if (g_ignore_validator_result) {
-      NaClLog(LOG_ERROR, "VALIDATION FAILED: continuing anyway...\n");
-      rcode = LOAD_OK;
-    } else {
-      NaClLog(LOG_ERROR, "VALIDATION FAILED.\n");
-      NaClLog(LOG_ERROR,
-              "Run sel_ldr in debug mode to ignore validation failure.\n");
-      NaClLog(LOG_ERROR,
-              "Run ncval <module-name> for validation error details.\n");
-      rcode = LOAD_VALIDATION_FAILED;
-    }
-  }
-  return rcode;
-}

src/trusted/service_runtime/arch/x86/service_runtime_x86.gyp
@@ -39,7 +39,6 @@
       ['target_base=="srt_x86_cmn"', {
         'sources': [
           'nacl_ldt_x86.c',
-          'sel_validate_image.c',
         ],
         'include_dirs': [
           '<(INTERMEDIATE_DIR)',

src/trusted/service_runtime/build.scons
@@ -87,6 +87,7 @@ ldr_inputs = [
     'sel_mem.c',
     'sel_util.c',
     'sel_util-inl.c',
+    'sel_validate_image.c',
     'web_worker_stub.c',
     'elf_util.c',
 ]
@@ -94,7 +95,6 @@ ldr_inputs = [
 if env['TARGET_ARCHITECTURE'] == 'x86':
   ldr_inputs += [
       'arch/x86/nacl_ldt_x86.c',
-      'arch/x86/sel_validate_image.c',
       ]
   if env['TARGET_SUBARCH'] == '32':
     ldr_inputs += [
@@ -129,7 +129,6 @@ elif env['BUILD_ARCHITECTURE'] == 'arm':
     'arch/arm/nacl_tls.c',
     'arch/arm/sel_ldr_arm.c',
     'arch/arm/sel_addrspace_arm.c',
-    'arch/arm/sel_validate_image.c',
     'arch/arm/nacl_switch.S',
     'arch/arm/nacl_syscall.S',
     'arch/arm/springboard.S',

src/trusted/service_runtime/sel_validate_image.c
@@ -1,0 +1,110 @@
+/*
+ * Copyright 2010 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include "native_client/src/shared/platform/nacl_log.h"
+#include "native_client/src/trusted/service_runtime/sel_ldr.h"
+
+
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86
+
+# if NACL_TARGET_SUBARCH == 32
+
+#  include "native_client/src/trusted/validator_x86/ncvalidate.h"
+
+int NaClValidateCode(struct NaClApp *nap, uint8_t *data, size_t size) {
+  struct NCValidatorState *vstate;
+  int validator_result;
+
+  vstate = NCValidateInit((uintptr_t) data, ((uintptr_t) data) + size,
+                          nap->bundle_size);
+  if (vstate == NULL) {
+    return LOAD_BAD_FILE;
+  }
+  NCValidateSegment(data, (uintptr_t) data, size, vstate);
+  validator_result = NCValidateFinish(vstate);
+  NCValidateFreeState(&vstate);
+  if (validator_result != 0) {
+    return LOAD_VALIDATION_FAILED;
+  }
+  return LOAD_OK;
+}
+
+# else
+
+#  include "native_client/src/trusted/validator_x86/ncvalidate_iter.h"
+
+int NaClValidateCode(struct NaClApp *nap, uint8_t *data, size_t size) {
+  struct NaClValidatorState *vstate;
+  int is_ok;
+
+  vstate = NaClValidatorStateCreate((uintptr_t) data, size, nap->bundle_size,
+                                    RegR15, 1, stderr);
+  if (vstate == NULL) {
+    return LOAD_BAD_FILE;
+  }
+  NaClValidateSegment(data, (uintptr_t) data, size, vstate);
+  is_ok = NaClValidatesOk(vstate);
+  NaClValidatorStateDestroy(vstate);
+  if (!is_ok) {
+    return LOAD_VALIDATION_FAILED;
+  }
+  return LOAD_OK;
+}
+
+# endif
+
+#elif NACL_ARCH(NACL_BUILD_ARCH) == NACL_arm
+
+# include "native_client/src/trusted/validator_arm/v2/ncvalidate.h"
+
+int NaClValidateCode(struct NaClApp *nap, uint8_t *data, size_t size) {
+  UNREFERENCED_PARAMETER(nap);
+
+  if (NCValidateSegment((uint8_t *) data, (uintptr_t) data, size) == 0) {
+    return LOAD_OK;
+  }
+  else {
+    return LOAD_VALIDATION_FAILED;
+  }
+}
+
+#endif
+
+static int g_ignore_validator_result = 0;
+
+void NaClIgnoreValidatorResult() {
+  g_ignore_validator_result = 1;
+}
+
+NaClErrorCode NaClValidateImage(struct NaClApp  *nap) {
+  uintptr_t               memp;
+  uintptr_t               endp;
+  size_t                  regionsize;
+  NaClErrorCode           rcode;
+
+  memp = nap->mem_start + NACL_TRAMPOLINE_END;
+  endp = nap->mem_start + nap->static_text_end;
+  regionsize = endp - memp;
+  if (endp < memp) {
+    return LOAD_NO_MEMORY;
+  }
+
+  rcode = NaClValidateCode(nap, (uint8_t *) memp, regionsize);
+  if (LOAD_OK != rcode) {
+    if (g_ignore_validator_result) {
+      NaClLog(LOG_ERROR, "VALIDATION FAILED: continuing anyway...\n");
+      rcode = LOAD_OK;
+    } else {
+      NaClLog(LOG_ERROR, "VALIDATION FAILED.\n");
+      NaClLog(LOG_ERROR,
+              "Run sel_ldr in debug mode to ignore validation failure.\n");
+      NaClLog(LOG_ERROR,
+              "Run ncval <module-name> for validation error details.\n");
+      rcode = LOAD_VALIDATION_FAILED;
+    }
+  }
+  return rcode;
+}

commit 3e8df18739e6839995ab1d1ed599aa0b89826251

Fix x86-64 Linux Gyp build (standalone and Chromium builds)
This should fix the build failure that occurs when building sel_validate_image.c:
./native_client/src/trusted/validator_x86/ncopcode_desc.h:18:72: error: gen/native_client/src/trusted/validator_x86/nacl_disallows.h: No such file or directory

The failure was introduced by r1969.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=318

Review URL: http://codereview.chromium.org/1609011

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1996 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/service_runtime.gyp
@@ -82,6 +82,11 @@
           'sel_validate_image.c',
           'web_worker_stub.c',
         ],
+        'include_dirs': [
+          # For generated header files from the x86-64 validator,
+          # e.g. nacl_disallows.h.
+          '<(SHARED_INTERMEDIATE_DIR)',
+        ],
         'sources!': [
            '<(syscall_handler)',
         ],