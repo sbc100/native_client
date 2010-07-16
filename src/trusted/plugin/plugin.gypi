sbc100/native_client

commit f54737272292cd6ef0d26223c9803fec1ca416c9

Plugin: Add asynchronous comms facility
Export two new IMC socket FDs from the plugin to the NaCl subprocess,
for sending and receiving messages.  (We use two rather than one to
work around issue 690 on Windows.)

Extend sel_ldr_launcher to support granting these FDs.  Remove
"is_sel_ldr_" because it is unused.

Add __setAsyncCallback() method for registering a callback that will
receive messages.  The thread it uses is implemented in
async_receive.cc.

Add __sendAsyncMessage0() and __sendAsyncMessage1() methods for
sending messages without/with an FD argument.

nacl_desc_wrapper.cc: Fix bug which causes an invalid memory access if
FDs are received but the FD buffer size is zero.

Remove gnu_inline from the definition of INLINE because it causes g++
on Ubuntu Jaunty to generate an external reference to
NaClRoundAllocPage() in npapi/video.cc.  While we could declare a
dependency of the plugin on the "sel" library, using gnu_inline is
brittle and causes unpredictable failures.  Omitting it gives the more
portable C99 behaviour.  For more background see:
BUG=http://code.google.com/p/nativeclient/issues/detail?id=697

Add "-z defs" to plugin build in order to catch missing symbols or
source files at link time on Linux.  This requires adding library
dependencies that were missing in the Gyp build.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=642
TEST=run_fake_browser_test

Review URL: http://codereview.chromium.org/2971005

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2708 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/plugin/plugin.gypi
@@ -52,10 +52,12 @@
       'srpc/srpc_client.cc',
       'srpc/srt_socket.cc',
       'srpc/stream_shm_buffer.cc',
+      'srpc/string_encoding.cc',
       'srpc/utility.cc',
     ],
     'npapi_sources': [
       # NPAPI specific code
+      'npapi/async_receive.cc',
       'npapi/browser_impl_npapi.cc',
       'npapi/closure.cc',
       'npapi/multimedia_socket.cc',
@@ -88,6 +90,15 @@
         ],
         'cflags': [
           '-Wno-long-long',
+        ],
+        'ldflags': [
+          # Catch unresolved symbols.
+          '-z', 'defs',
+        ],
+        'libraries': [
+          '-ldl',
+          '-lX11',
+          '-lXt',
         ],
       }],
       ['OS=="mac"', {

src/include/portability.h
@@ -68,11 +68,7 @@
 #if NACL_WINDOWS
 # define INLINE __forceinline
 #else
-# if __GNUC_MINOR__ >= 2
-#  define INLINE __inline__ __attribute__((gnu_inline))
-# else
-#  define INLINE __inline__
-# endif
+# define INLINE __inline__
 #endif
 
 #if NACL_WINDOWS

src/trusted/desc/nacl_desc_wrapper.cc
@@ -585,7 +585,7 @@ ssize_t DescWrapper::RecvMsg(MsgHeader* dgram, int flags) {
   if (NULL == header.ndescv) {
     goto cleanup;
   }
-  header.ndesc_length = diov_length;
+  header.ndesc_length = ddescv_length;
   // Receive the message.
   ret = NaClImcRecvTypedMessage(desc_, common_data_->effp(), &header, flags);
   if (ret < 0) {

src/trusted/nonnacl_util/linux/sel_ldr_launcher_linux.cc
@@ -18,6 +18,7 @@
 #include <unistd.h>
 
 #include "native_client/src/include/nacl_string.h"
+#include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/trusted/nonnacl_util/sel_ldr_launcher.h"
 #include "native_client/src/trusted/desc/nacl_desc_base.h"
@@ -28,6 +29,7 @@ using std::vector;
 namespace nacl {
 
 SelLdrLauncher::~SelLdrLauncher() {
+  CloseHandlesAfterLaunch();
   // sock_addr_ is non-NULL only if the Start method successfully completes.
   if (NULL != sock_addr_) {
     NaClDescUnref(sock_addr_);
@@ -51,6 +53,21 @@ nacl::string SelLdrLauncher::GetSelLdrPathName() {
   char buffer[FILENAME_MAX];
   GetPluginDirectory(buffer, sizeof(buffer));
   return nacl::string(buffer) + "/sel_ldr";
+}
+
+Handle SelLdrLauncher::ExportImcFD(int dest_fd) {
+  Handle pair[2];
+  if (SocketPair(pair) == -1) {
+    return kInvalidHandle;
+  }
+
+  int rc = fcntl(pair[0], F_SETFD, FD_CLOEXEC);
+  CHECK(rc == 0);
+  close_after_launch_.push_back(pair[1]);
+
+  sel_ldr_argv_.push_back("-i");
+  sel_ldr_argv_.push_back(ToString(dest_fd) + ":" + ToString(pair[1]));
+  return pair[0];
 }
 
 const size_t kMaxExecArgs = 64;
@@ -103,6 +120,7 @@ bool SelLdrLauncher::Launch() {
     perror("execv");
     _exit(EXIT_FAILURE);
   }
+  CloseHandlesAfterLaunch();
   Close(pair[1]);
   channel_ = pair[0];
 

src/trusted/nonnacl_util/sel_ldr_launcher.cc
@@ -17,13 +17,6 @@
 
 using std::vector;
 
-// TODO(robertm): Move this to  new header if it becomes more popular
-template <class T> nacl::string ToString(const T& t) {
-  nacl::stringstream ss;
-  ss << t;
-  return ss.str();
-}
-
 
 namespace nacl {
 
@@ -31,7 +24,6 @@ SelLdrLauncher::SelLdrLauncher()
   : child_(kInvalidHandle),
     channel_(kInvalidHandle),
     sock_addr_(NULL),
-    is_sel_ldr_(true),
     sel_ldr_locator_(new PluginSelLdrLocator()) {
 }
 
@@ -39,7 +31,6 @@ SelLdrLauncher::SelLdrLauncher(SelLdrLocator* sel_ldr_locator)
   : child_(kInvalidHandle),
     channel_(kInvalidHandle),
     sock_addr_(NULL),
-    is_sel_ldr_(true),
     sel_ldr_locator_(sel_ldr_locator) {
 }
 
@@ -232,6 +223,13 @@ void SelLdrLauncher::InitChannelBuf(Handle imc_channel_handle) {
 #else
                  ToString(imc_channel_handle);
 #endif
+}
+
+void SelLdrLauncher::CloseHandlesAfterLaunch() {
+  for(size_t i = 0; i < close_after_launch_.size(); i++) {
+    Close(close_after_launch_[i]);
+  }
+  close_after_launch_.clear();
 }
 
 }  // namespace nacl

src/trusted/nonnacl_util/sel_ldr_launcher.h
@@ -21,6 +21,13 @@ struct NaClDesc;
 
 namespace nacl {
 
+// TODO(robertm): Move this to new header if it becomes more popular.
+template <class T> nacl::string ToString(const T& t) {
+  nacl::stringstream ss;
+  ss << t;
+  return ss.str();
+}
+
 /*
  * This class defines an interface that locates sel_ldr.
  */
@@ -61,11 +68,26 @@ struct SelLdrLauncher {
 
   explicit SelLdrLauncher(SelLdrLocator* sel_ldr_locator);
 
+  // Creates a socket pair.  Maps the first socket into the NaCl
+  // subprocess's FD table as dest_fd.  Returns the second socket.
+  // Returns kInvalidHandle on failure.
+  Handle ExportImcFD(int dest_fd);
+
   // TODO(robertm): shouldn't some of these args go in the constructor
   // application_argv can take a "$CHAN" as an argument which will be replaced
   // with `imc_fd` number if the target application is a NaCl module
   // and with `channel_` number if the target application is a native OS
   // binary.
+
+  void Init(const nacl::string& application_name,
+            int imc_fd,
+            const std::vector<nacl::string>& sel_ldr_argv,
+            const std::vector<nacl::string>& application_argv);
+
+  // If subprocess creation fails, both child_ and channel_ are set to
+  // kInvalidHandle. We have different implementations for unix and win.
+  // NOTE: you must call Init() and InitChannelBuf() before Launch()
+  bool Launch();
 
   bool Start(const nacl::string& application_name,
              int imc_fd,
@@ -75,19 +97,6 @@ struct SelLdrLauncher {
     return Launch();
   }
 
-  // Obsolete: compatibility interface
-  bool Start(const nacl::string& application_name,
-             int imc_fd,
-             int sel_ldr_argc,
-             const char* sel_ldr_argv[],
-             int app_argc,
-             const char* app_argv[]) {
-    const std::vector<nacl::string> sel_ldr_vec(sel_ldr_argv,
-                                               sel_ldr_argv + sel_ldr_argc);
-    const std::vector<nacl::string> app_vec(app_argv,
-                                           app_argv + app_argc);
-    return Start(application_name, imc_fd, sel_ldr_vec, app_vec);
-  }
   // TODO(gregoryd): add some background story here
 #ifdef NACL_STANDALONE
   // Should never be called when not running in Chrome.
@@ -122,11 +131,6 @@ struct SelLdrLauncher {
   // Returns the socket address used to connect to the sel_ldr.
   struct NaClDesc* GetSelLdrSocketAddress() const { return sock_addr_; }
 
-  // Returns true if SelLdrLauncher has launched an instance of sel_ldr, or
-  // false if SelLdrLauncher has launched a native OS binary instead of sel_ldr
-  // for debugging.
-  bool IsSelLdr() const { return is_sel_ldr_; }
-
   // Kill the child process.  The channel() remains valid, but nobody
   // is talking on the other end.  Returns true if successful.
   bool KillChild();
@@ -140,19 +144,10 @@ struct SelLdrLauncher {
 
   void BuildArgv(std::vector<nacl::string>* argv);
 
-  // If subprocess creation fails, both child_ and channel_ are set to
-  // kInvalidHandle. We have different implementations for unix and win.
-  // NOTE: you must call Init() and InitChannelBuf() before Launch()
-  bool Launch();
-
-  void Init(const nacl::string& application_name,
-            int imc_fd,
-            const std::vector<nacl::string>& sel_ldr_argv,
-            const std::vector<nacl::string>& application_argv);
-
   void InitChannelBuf(Handle handle);
 
- private:
+  void CloseHandlesAfterLaunch();
+
   Handle child_;
   Handle channel_;
   // The following strings and vectors are used by BuildArgv to
@@ -164,14 +159,10 @@ struct SelLdrLauncher {
   std::vector<nacl::string> application_argv_;
   // unlike the others above this is set from within Launch();
   nacl::string channel_buf_;
+  std::vector<Handle> close_after_launch_;
 
   // The socket address returned from sel_ldr for connects.
   struct NaClDesc* sock_addr_;
-
-  // The following boolean value is set to true if SelLdrLauncher has launched
-  // an instance of sel_ldr, or false if child_ is executing a native OS binary
-  // instead of sel_ldr for debugging.
-  bool is_sel_ldr_;
 
   SelLdrLocator* sel_ldr_locator_;
 };

src/trusted/nonnacl_util/sel_ldr_launcher_chrome.cc
@@ -26,6 +26,7 @@ namespace nacl {
     NaClHandlePassBrowserRememberHandle(nacl_proc_id, nacl_proc_handle);
 #endif
 
+    CloseHandlesAfterLaunch();
     // TODO(gregoryd): the handle is currently returned on Windows only.
     child_ = nacl_proc_handle;
     // The handle we get back is the plugins end of the initial communication

src/trusted/nonnacl_util/win/sel_ldr_launcher_win.cc
@@ -24,6 +24,7 @@ using std::vector;
 namespace nacl {
 
 SelLdrLauncher::~SelLdrLauncher() {
+  CloseHandlesAfterLaunch();
   if (NULL != sock_addr_) {
     NaClDescUnref(sock_addr_);
   }
@@ -67,6 +68,34 @@ static nacl::string Escape(nacl::string s) {
   return result;
 }
 
+Handle SelLdrLauncher::ExportImcFD(int dest_fd) {
+  Handle pair[2];
+  if (SocketPair(pair) == -1) {
+    return kInvalidHandle;
+  }
+
+  // Transfer pair[1] to child_ by inheritance.
+  // TODO(mseaborn): We could probably use SetHandleInformation() to
+  // set HANDLE_FLAG_INHERIT rather than duplicating the handle and
+  // closing the original.  But for now, we do the same as in Launch()
+  // below.
+  Handle channel;
+  if (!DuplicateHandle(GetCurrentProcess(), pair[1],
+                       GetCurrentProcess(), &channel,
+                       0, TRUE, DUPLICATE_SAME_ACCESS)) {
+    Close(pair[0]);
+    Close(pair[1]);
+    return kInvalidHandle;
+  }
+  Close(pair[1]);
+  close_after_launch_.push_back(channel);
+
+  sel_ldr_argv_.push_back("-i");
+  sel_ldr_argv_.push_back(ToString(dest_fd) + ":" +
+                          ToString(reinterpret_cast<uintptr_t>(channel)));
+  return pair[0];
+}
+
 bool SelLdrLauncher::Launch() {
   Handle pair[2];
   STARTUPINFOA startup_info;
@@ -77,6 +106,7 @@ bool SelLdrLauncher::Launch() {
   }
 
   // Transfer pair[1] to child_ by an inheritance.
+  // TODO(mseaborn): This could use ExportImcFD().
   Handle channel;
   if (!DuplicateHandle(GetCurrentProcess(), pair[1],
                        GetCurrentProcess(), &channel,
@@ -114,6 +144,7 @@ bool SelLdrLauncher::Launch() {
     return false;
   }
 
+  CloseHandlesAfterLaunch();
   Close(pair[1]);
   Close(channel);
   CloseHandle(process_infomation.hThread);

commit 2a3cfa65819b4b140f2bf135da03a81e5ce4ff59

Plugin: Fix Gyp Linux-ARM build by passing "-z defs" in properly
Fix breakage introduced by r2708.  Not all versions of gcc will pass
"-z defs" through to the linker, so wrap it with "-Wl".

BUG=http://code.google.com/p/nativeclient/issues/detail?id=642
TEST=check command line printed by "make V=1" on Gyp build

Review URL: http://codereview.chromium.org/3042001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2709 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/plugin/plugin.gypi
@@ -93,7 +93,7 @@
         ],
         'ldflags': [
           # Catch unresolved symbols.
-          '-z', 'defs',
+          '-Wl,-z,defs',
         ],
         'libraries': [
           '-ldl',