sbc100/native_client

commit 8b3f5c45c582ec3cc22d90a8ccf4babac436b887

IMC: Remove ReturnCreatedDesc() method from effector object
Change the ConnectAddr() and AcceptConn() methods to return a NaClDesc
rather than a descriptor number.

This means the syscall function is now responsible for adding the
descriptor into the FD table with NaClSetAvail(), which is consistent
with the other syscall functions.

This means we can remove NaClNrdXferEffectorDtor(), which was a
workaround for cases where a NaClDesc was what the caller wanted from
ConnectAddr()/AcceptConn().

This removes the need to create effector objects in some cases.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=727
TEST=existing tests

Review URL: http://codereview.chromium.org/3076010

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2827 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/handle_pass/ldr_handle.cc
@@ -32,47 +32,27 @@ void NaClHandlePassLdrInit() {
 int NaClHandlePassLdrCtor(struct NaClDesc* socket_address,
                           DWORD renderer_pid,
                           NaClHandle renderer_handle) {
-  struct NaClNrdXferEffector effector;
-  struct NaClDescEffector* effp;
-  int retval = 0;
-
   NaClMutexLock(&pid_handle_map_mu);
   local_pid_handle_map = new(std::nothrow) std::map<DWORD, HANDLE>;
   if (NULL == local_pid_handle_map) {
-    retval = 0;
     NaClMutexUnlock(&pid_handle_map_mu);
-    goto no_state;
+    return 0;
   }
   (*local_pid_handle_map)[renderer_pid] = renderer_handle;
   NaClMutexUnlock(&pid_handle_map_mu);
   NaClHandlePassSetLookupMode(HANDLE_PASS_CLIENT_PROCESS);
 
-  // Create an effector to use to receive the connected socket.
-  if (!NaClNrdXferEffectorCtor(&effector)) {
-    goto no_state;
-  }
-  effp = reinterpret_cast<struct NaClDescEffector*>(&effector);
   // Connect to the given socket address.
-  if (0 != (socket_address->vtbl->ConnectAddr)(socket_address, effp)) {
-    goto effector_constructed;
+  if (0 == (socket_address->vtbl->ConnectAddr)(socket_address, &lookup_desc)) {
+    return 0;
   }
-  // Get the connected socket from the effector.
-  lookup_desc = NaClNrdXferEffectorTakeDesc(&effector);
   // Create an SRPC client for lookup requests.
   if (!NaClSrpcClientCtor(&lookup_channel, lookup_desc)) {
-    goto error_connected;
+    NaClDescUnref(lookup_desc);
+    return 0;
   }
-  // Success.  Clean up everything but lookup_channel and lookup_desc.
-  retval = 1;
-  goto effector_constructed;
-
- error_connected:
-  NaClDescUnref(lookup_desc);
- effector_constructed:
-  // Clean up the effector.
-  effp->vtbl->Dtor(effp);
- no_state:
-  return retval;
+  // Success.
+  return 1;
 }
 
 HANDLE NaClHandlePassLdrLookupHandle(DWORD pid) {

src/trusted/desc/nacl_desc_base.c
@@ -444,9 +444,9 @@ ssize_t NaClDescRecvMsgNotImplemented(struct NaClDesc           *vself,
   return -NACL_ABI_EINVAL;
 }
 
-int NaClDescConnectAddrNotImplemented(struct NaClDesc         *vself,
-                                      struct NaClDescEffector *effp) {
-  UNREFERENCED_PARAMETER(effp);
+int NaClDescConnectAddrNotImplemented(struct NaClDesc *vself,
+                                      struct NaClDesc **result) {
+  UNREFERENCED_PARAMETER(result);
 
   NaClLog(LOG_ERROR,
           "ConnectAddr method is not implemented for object of type %s\n",
@@ -454,9 +454,9 @@ int NaClDescConnectAddrNotImplemented(struct NaClDesc         *vself,
   return -NACL_ABI_EINVAL;
 }
 
-int NaClDescAcceptConnNotImplemented(struct NaClDesc          *vself,
-                                     struct NaClDescEffector  *effp) {
-  UNREFERENCED_PARAMETER(effp);
+int NaClDescAcceptConnNotImplemented(struct NaClDesc *vself,
+                                     struct NaClDesc **result) {
+  UNREFERENCED_PARAMETER(result);
 
   NaClLog(LOG_ERROR,
           "AcceptConn method is not implemented for object of type %s\n",

src/trusted/desc/nacl_desc_base.h
@@ -314,11 +314,15 @@ struct NaClDescVtbl {
                      struct NaClMessageHeader *dgram,
                      int                      flags) NACL_WUR;
 
-  int (*ConnectAddr)(struct NaClDesc          *vself,
-                     struct NaClDescEffector  *effp) NACL_WUR;
-
-  int (*AcceptConn)(struct NaClDesc         *vself,
-                    struct NaClDescEffector *effp) NACL_WUR;
+  /*
+   * ConnectAddr() and AcceptConn():
+   * On success, returns 0 and a descriptor via *result.
+   * On error, returns a negative errno value.
+   */
+  int (*ConnectAddr)(struct NaClDesc *vself,
+                     struct NaClDesc **result) NACL_WUR;
+  int (*AcceptConn)(struct NaClDesc *vself,
+                    struct NaClDesc **result) NACL_WUR;
 
   int (*Post)(struct NaClDesc         *vself,
               struct NaClDescEffector *effp) NACL_WUR;
@@ -659,10 +663,10 @@ ssize_t NaClDescRecvMsgNotImplemented(struct NaClDesc           *vself,
                                   struct NaClDescEffector   *effp,
                                   struct NaClMessageHeader  *dgram,
                                   int                       flags);
-int NaClDescConnectAddrNotImplemented(struct NaClDesc         *vself,
-                                      struct NaClDescEffector *effp);
-int NaClDescAcceptConnNotImplemented(struct NaClDesc          *vself,
-                                     struct NaClDescEffector  *effp);
+int NaClDescConnectAddrNotImplemented(struct NaClDesc *vself,
+                                      struct NaClDesc **result);
+int NaClDescAcceptConnNotImplemented(struct NaClDesc *vself,
+                                     struct NaClDesc **result);
 int NaClDescPostNotImplemented(struct NaClDesc          *vself,
                                struct NaClDescEffector  *effp);
 int NaClDescSemWaitNotImplemented(struct NaClDesc         *vself,

src/trusted/desc/nacl_desc_conn_cap.c
@@ -87,8 +87,8 @@ int NaClDescConnCapExternalize(struct NaClDesc          *vself,
   return 0;
 }
 
-int NaClDescConnCapConnectAddr(struct NaClDesc          *vself,
-                               struct NaClDescEffector  *effp) {
+int NaClDescConnCapConnectAddr(struct NaClDesc *vself,
+                               struct NaClDesc **result) {
   /*
    * See NaClDescImcBoundDescAcceptConn code in
    * nacl_desc_imc_bound_desc.c
@@ -143,12 +143,8 @@ int NaClDescConnCapConnectAddr(struct NaClDesc          *vself,
   }
   nh[1] = NACL_INVALID_HANDLE;
 
-  retval = (*effp->vtbl->ReturnCreatedDesc)(effp, ((struct NaClDesc *) peer));
-  if (retval < 0) {
-    /* peer is fully constructed, so we cannot simply free it at this pt */
-    NaClDescUnref((struct NaClDesc *) peer);
-    peer = NULL;
-  }
+  *result = (struct NaClDesc *) peer;
+  retval = 0;
 
 cleanup:
   if (retval < 0) {
@@ -163,10 +159,10 @@ cleanup:
   return retval;
 }
 
-int NaClDescConnCapAcceptConn(struct NaClDesc         *vself,
-                              struct NaClDescEffector *effp) {
+int NaClDescConnCapAcceptConn(struct NaClDesc *vself,
+                              struct NaClDesc **result) {
   UNREFERENCED_PARAMETER(vself);
-  UNREFERENCED_PARAMETER(effp);
+  UNREFERENCED_PARAMETER(result);
 
   NaClLog(LOG_ERROR, "NaClDescConnCapAcceptConn: not IMC\n");
   return -NACL_ABI_EINVAL;

src/trusted/desc/nacl_desc_effector.h
@@ -43,32 +43,6 @@ struct NaClDescEffectorVtbl {
   void (*Dtor)(struct NaClDescEffector *vself);
 
   /*
-   * Return a constructed NaClDesc object to the calling environment
-   * through the effector interface.  Takes ownership of the NaClDesc
-   * object.
-   *
-   * For the service runtime, this insert the newly created NaClDesc
-   * object into open-file table, returning index (descriptor).
-   *
-   * For trusted application code, each call returns a NaClDesc, but
-   * in methods that may return more than one, dynamic cast style
-   * checks are needed to determine which NaClDesc is which (assuming
-   * that their types differ), or be determined by the order in which
-   * they'red produced (which is fragile, since the implementation
-   * might change the order in which the NaClDesc objects are
-   * produced).  The returned indices are used for the return value
-   * from the NaClDesc virtual function (or as output arguments) and
-   * should not be negative; otherwise the value is immaterial.
-   *
-   * For example, NaClDescConnCapConnectAddr and NaClDescImcBoundDesc
-   * uses this method since they are factories that produce
-   * NaClDescImcDesc objects.
-   */
-  int (*ReturnCreatedDesc)(struct NaClDescEffector  *vself,
-                           struct NaClDesc          *ndp);
-
-
-  /*
    * For service runtime, the NaClDesc's Map virtual function will
    * call this to unmap any existing memory before mapping new pages
    * in on top.  This method should handle the necessary unmapping

src/trusted/desc/nacl_desc_effector_cleanup.c
@@ -25,14 +25,6 @@ static void NaClDescEffCleanDtor(struct NaClDescEffector *vself) {
   return;
 }
 
-static int NaClDescEffCleanReturnCreatedDesc(struct NaClDescEffector *vself,
-                                             struct NaClDesc         *ndp) {
-  UNREFERENCED_PARAMETER(vself);
-  UNREFERENCED_PARAMETER(ndp);
-  NaClLog(LOG_FATAL, "Cleanup effector's ReturnCreatedDesc called\n");
-  return 0;
-}
-
 static int NaClDescEffCleanUnmapMemory(struct NaClDescEffector  *vself,
                                        uintptr_t                sysaddr,
                                        size_t                   nbytes) {
@@ -57,7 +49,6 @@ static uintptr_t NaClDescEffCleanMapAnonMem(struct NaClDescEffector *vself,
 
 static struct NaClDescEffectorVtbl NaClDescEffectorCleanupVtbl = {
   NaClDescEffCleanDtor,
-  NaClDescEffCleanReturnCreatedDesc,
   NaClDescEffCleanUnmapMemory,
   NaClDescEffCleanMapAnonMem,
 };

src/trusted/desc/nacl_desc_effector_trusted_mem.c
@@ -26,15 +26,6 @@ static void NaClDescEffTrustedMemDtor(struct NaClDescEffector *vself) {
   return;
 }
 
-static int NaClDescEffTrustedMemReturnCreatedDesc(
-    struct NaClDescEffector *vself,
-    struct NaClDesc         *ndp) {
-  UNREFERENCED_PARAMETER(vself);
-  UNREFERENCED_PARAMETER(ndp);
-  NaClLog(LOG_FATAL, "TrustedMem effector's ReturnCreatedDesc called\n");
-  return 0;
-}
-
 static int NaClDescEffTrustedMemUnmapMemory(struct NaClDescEffector  *vself,
                                             uintptr_t                sysaddr,
                                             size_t                   nbytes) {
@@ -60,7 +51,6 @@ static uintptr_t NaClDescEffTrustedMemMapAnonMem(
 
 static struct NaClDescEffectorVtbl NaClDescEffectorTrustedMemVtbl = {
   NaClDescEffTrustedMemDtor,
-  NaClDescEffTrustedMemReturnCreatedDesc,
   NaClDescEffTrustedMemUnmapMemory,
   NaClDescEffTrustedMemMapAnonMem,
 };

src/trusted/desc/nacl_desc_imc_bound_desc.c
@@ -69,8 +69,8 @@ int NaClDescImcBoundDescClose(struct NaClDesc         *vself,
   return 0;
 }
 
-int NaClDescImcBoundDescAcceptConn(struct NaClDesc          *vself,
-                                   struct NaClDescEffector  *effp) {
+int NaClDescImcBoundDescAcceptConn(struct NaClDesc *vself,
+                                   struct NaClDesc **result) {
   /*
    * See NaClDescConnCapConnectAddr code in nacl_desc_conn_cap.c
    */
@@ -95,11 +95,9 @@ int NaClDescImcBoundDescAcceptConn(struct NaClDesc          *vself,
   nh = NACL_INVALID_HANDLE;
 
   NaClLog(3,
-          ("NaClDescImcBoundDescAcceptConn(0x%08"NACL_PRIxPTR", "
-           "0x%08"NACL_PRIxPTR"):"
+          ("NaClDescImcBoundDescAcceptConn(0x%08"NACL_PRIxPTR"):"
            " h = %d\n"),
           (uintptr_t) vself,
-          (uintptr_t) effp,
           self->h);
 
   if (-1 == (nbytes = NaClReceiveDatagram(self->h, &conn_msg, 0))) {
@@ -129,11 +127,8 @@ int NaClDescImcBoundDescAcceptConn(struct NaClDesc          *vself,
   }
   nh = NACL_INVALID_HANDLE;
 
-  retval = (*effp->vtbl->ReturnCreatedDesc)(effp, (struct NaClDesc *) peer);
-
-  if (retval < 0) {
-    (*peer->base.base.vtbl->Dtor)((struct NaClDesc *) peer);
-  }
+  *result = (struct NaClDesc *) peer;
+  retval = 0;
 
 cleanup:
   if (retval < 0) {

commit 24d60069091521c43a1e0c5ec55143deb68f6256

handle_pass: Fix mistake in ConnectAddr() call
Fix mistake, introduced in r2827, in order to fix nacl_ui_tests on
Windows.  Check the return value properly.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=727
TEST=nacl_ui_tests on Windows

Review URL: http://codereview.chromium.org/2819088

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2881 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/handle_pass/ldr_handle.cc
@@ -43,7 +43,7 @@ int NaClHandlePassLdrCtor(struct NaClDesc* socket_address,
   NaClHandlePassSetLookupMode(HANDLE_PASS_CLIENT_PROCESS);
 
   // Connect to the given socket address.
-  if (0 == (socket_address->vtbl->ConnectAddr)(socket_address, &lookup_desc)) {
+  if (0 != (socket_address->vtbl->ConnectAddr)(socket_address, &lookup_desc)) {
     return 0;
   }
   // Create an SRPC client for lookup requests.