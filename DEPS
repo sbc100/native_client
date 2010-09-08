sbc100/native_client

commit 4b237a1d2d99343c46766548e196414bbe80b28e

Update ppapi revision to 235. Add all new .cc and .h files from ppapi.gyp to .scons targets. Update ppapi_proxy code that no longer compiles. Disable the gyp build for now.
Review URL: http://codereview.chromium.org/3292013

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3222 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

DEPS
@@ -14,7 +14,7 @@ vars = {
   "tools_rev": "3082",
   "python_rev": "22967",
   "wix_rev": "153",
-  "ppapi_rev": "197",
+  "ppapi_rev": "235",
   "lcov_rev": "54822",
 }
 

build/all.gyp
@@ -8,7 +8,7 @@
       'type': 'none',
       # NOTE: Chrome-specific targets should not be part of this project
       'dependencies': [
-        '../../ppapi/ppapi.gyp:*',
+#        '../../ppapi/ppapi.gyp:*',
         '../src/shared/gio/gio.gyp:*',
         '../src/shared/imc/imc.gyp:*',
         '../src/shared/npruntime/npruntime.gyp:*',

src/shared/ppapi/build.scons
@@ -13,19 +13,9 @@ if env.Bit('windows'):
 
 cpp_sources = [
     # from ppapi.gyp:ppapi_cpp_objects
-    '$SOURCE_ROOT/ppapi/cpp/audio.cc',
-    '$SOURCE_ROOT/ppapi/cpp/audio_config.cc',
-    '$SOURCE_ROOT/ppapi/cpp/buffer.cc',
     '$SOURCE_ROOT/ppapi/cpp/core.cc',
-    '$SOURCE_ROOT/ppapi/cpp/device_context_2d.cc',
-    '$SOURCE_ROOT/ppapi/cpp/directory_entry.cc',
-    '$SOURCE_ROOT/ppapi/cpp/directory_reader.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_chooser.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_io.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_ref.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_system.cc',
+    '$SOURCE_ROOT/ppapi/cpp/graphics_2d.cc',
     '$SOURCE_ROOT/ppapi/cpp/image_data.cc',
-    '$SOURCE_ROOT/ppapi/cpp/font.cc',
     '$SOURCE_ROOT/ppapi/cpp/instance.cc',
     '$SOURCE_ROOT/ppapi/cpp/module.cc',
     '$SOURCE_ROOT/ppapi/cpp/paint_aggregator.cc',
@@ -33,14 +23,26 @@ cpp_sources = [
     '$SOURCE_ROOT/ppapi/cpp/rect.cc',
     '$SOURCE_ROOT/ppapi/cpp/resource.cc',
     '$SOURCE_ROOT/ppapi/cpp/scriptable_object.cc',
-    '$SOURCE_ROOT/ppapi/cpp/scrollbar.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_loader.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_request_info.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_response_info.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_util.cc',
     '$SOURCE_ROOT/ppapi/cpp/var.cc',
-    '$SOURCE_ROOT/ppapi/cpp/video_decoder.cc',
-    '$SOURCE_ROOT/ppapi/cpp/widget.cc',
+    # Dev interfaces.
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_config_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/buffer_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_entry_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_reader_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_chooser_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_io_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_ref_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_system_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/font_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/graphics_3d_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/scrollbar_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_loader_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_request_info_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_response_info_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_util_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/video_decoder_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/widget_dev.cc',
     # from ppapi.gyp::ppapi_cpp
     '$SOURCE_ROOT/ppapi/cpp/ppp_entrypoints.cc',
     ]

src/shared/ppapi/nacl.scons
@@ -9,19 +9,9 @@ Import('env')
 
 cpp_sources = [
     # from ppapi.gyp:ppapi_cpp_objects
-    '$SOURCE_ROOT/ppapi/cpp/audio.cc',
-    '$SOURCE_ROOT/ppapi/cpp/audio_config.cc',
-    '$SOURCE_ROOT/ppapi/cpp/buffer.cc',
     '$SOURCE_ROOT/ppapi/cpp/core.cc',
-    '$SOURCE_ROOT/ppapi/cpp/device_context_2d.cc',
-    '$SOURCE_ROOT/ppapi/cpp/directory_entry.cc',
-    '$SOURCE_ROOT/ppapi/cpp/directory_reader.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_chooser.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_io.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_ref.cc',
-    '$SOURCE_ROOT/ppapi/cpp/file_system.cc',
+    '$SOURCE_ROOT/ppapi/cpp/graphics_2d.cc',
     '$SOURCE_ROOT/ppapi/cpp/image_data.cc',
-    '$SOURCE_ROOT/ppapi/cpp/font.cc',
     '$SOURCE_ROOT/ppapi/cpp/instance.cc',
     '$SOURCE_ROOT/ppapi/cpp/module.cc',
     '$SOURCE_ROOT/ppapi/cpp/paint_aggregator.cc',
@@ -29,14 +19,26 @@ cpp_sources = [
     '$SOURCE_ROOT/ppapi/cpp/rect.cc',
     '$SOURCE_ROOT/ppapi/cpp/resource.cc',
     '$SOURCE_ROOT/ppapi/cpp/scriptable_object.cc',
-    '$SOURCE_ROOT/ppapi/cpp/scrollbar.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_loader.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_request_info.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_response_info.cc',
-    '$SOURCE_ROOT/ppapi/cpp/url_util.cc',
     '$SOURCE_ROOT/ppapi/cpp/var.cc',
-    '$SOURCE_ROOT/ppapi/cpp/video_decoder.cc',
-    '$SOURCE_ROOT/ppapi/cpp/widget.cc',
+    # Dev interfaces.
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_config_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/buffer_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_entry_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_reader_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_chooser_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_io_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_ref_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_system_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/font_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/graphics_3d_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/scrollbar_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_loader_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_request_info_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_response_info_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_util_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/video_decoder_dev.cc',
+    '$SOURCE_ROOT/ppapi/cpp/dev/widget_dev.cc',
     # from ppapi.gyp::ppapi_cpp
     '$SOURCE_ROOT/ppapi/cpp/ppp_entrypoints.cc',
     ]
@@ -46,10 +48,8 @@ env.ComponentLibrary('ppapi_cpp', cpp_sources)
 c_headers = [
     # from ppapi.gyp:ppapi_c
     '$SOURCE_ROOT/ppapi/c/pp_completion_callback.h',
-    '$SOURCE_ROOT/ppapi/c/pp_cursor_type.h',
     '$SOURCE_ROOT/ppapi/c/pp_errors.h',
     '$SOURCE_ROOT/ppapi/c/pp_event.h',
-    '$SOURCE_ROOT/ppapi/c/pp_file_info.h',
     '$SOURCE_ROOT/ppapi/c/pp_instance.h',
     '$SOURCE_ROOT/ppapi/c/pp_module.h',
     '$SOURCE_ROOT/ppapi/c/pp_point.h',
@@ -58,73 +58,86 @@ c_headers = [
     '$SOURCE_ROOT/ppapi/c/pp_size.h',
     '$SOURCE_ROOT/ppapi/c/pp_stdint.h',
     '$SOURCE_ROOT/ppapi/c/pp_var.h',
-    '$SOURCE_ROOT/ppapi/c/pp_video.h',
     '$SOURCE_ROOT/ppapi/c/ppb.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_audio.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_audio_config.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_audio_trusted.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_buffer.h',
     '$SOURCE_ROOT/ppapi/c/ppb_core.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_device_context_2d.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_directory_reader.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_file_chooser.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_file_io.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_file_io_trusted.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_file_ref.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_file_system.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_find.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_font.h',
     '$SOURCE_ROOT/ppapi/c/ppb_image_data.h',
     '$SOURCE_ROOT/ppapi/c/ppb_instance.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_testing.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_url_loader.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_url_request_info.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_url_response_info.h',
-    '$SOURCE_ROOT/ppapi/c/ppb_url_util.h',
     '$SOURCE_ROOT/ppapi/c/ppb_var.h',
     '$SOURCE_ROOT/ppapi/c/ppp.h',
     '$SOURCE_ROOT/ppapi/c/ppp_class.h',
     '$SOURCE_ROOT/ppapi/c/ppp_instance.h',
-    '$SOURCE_ROOT/ppapi/c/ppp_printing.h',
-    '$SOURCE_ROOT/ppapi/c/ppp_scrollbar.h',
-    '$SOURCE_ROOT/ppapi/c/ppp_widget.h',
-    '$SOURCE_ROOT/ppapi/c/ppp_zoom.h',
+    # Dev interfaces.
+    '$SOURCE_ROOT/ppapi/c/dev/pp_cursor_type_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/pp_file_info_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/pp_video_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_audio_config_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_audio_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_audio_trusted_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_buffer_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_char_set_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_cursor_control_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_directory_reader_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_file_chooser_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_file_io_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_file_io_trusted_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_file_ref_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_file_system_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_find_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_font_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_graphics_2d_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_graphics_3d_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_opengles_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_scrollbar_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_testing_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_url_loader_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_url_request_info_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_url_response_info_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_url_util_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppb_video_decoder_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_cursor_control_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_find_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_graphics_3d_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_printing_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_widget_dev.h',
+    '$SOURCE_ROOT/ppapi/c/dev/ppp_zoom_dev.h',
     ]
 
 cpp_headers = [
     # from  ppapi.gyp:ppapi_cpp_objects
-    '$SOURCE_ROOT/ppapi/cpp/audio.h',
-    '$SOURCE_ROOT/ppapi/cpp/audio_config.h',
-    '$SOURCE_ROOT/ppapi/cpp/buffer.h',
     '$SOURCE_ROOT/ppapi/cpp/completion_callback.h',
     '$SOURCE_ROOT/ppapi/cpp/core.h',
-    '$SOURCE_ROOT/ppapi/cpp/device_context_2d.h',
-    '$SOURCE_ROOT/ppapi/cpp/directory_entry.h',
-    '$SOURCE_ROOT/ppapi/cpp/directory_reader.h',
-    '$SOURCE_ROOT/ppapi/cpp/file_chooser.h',
-    '$SOURCE_ROOT/ppapi/cpp/file_io.h',
-    '$SOURCE_ROOT/ppapi/cpp/file_ref.h',
-    '$SOURCE_ROOT/ppapi/cpp/file_system.h',
-    '$SOURCE_ROOT/ppapi/cpp/font.h',
+    '$SOURCE_ROOT/ppapi/cpp/graphics_2d.h',
     '$SOURCE_ROOT/ppapi/cpp/image_data.h',
     '$SOURCE_ROOT/ppapi/cpp/instance.h',
     '$SOURCE_ROOT/ppapi/cpp/module.h',
-    '$SOURCE_ROOT/ppapi/cpp/paint_manager.h',
+    '$SOURCE_ROOT/ppapi/cpp/module_impl.h',
     '$SOURCE_ROOT/ppapi/cpp/paint_aggregator.h',
+    '$SOURCE_ROOT/ppapi/cpp/paint_manager.h',
     '$SOURCE_ROOT/ppapi/cpp/point.h',
     '$SOURCE_ROOT/ppapi/cpp/rect.h',
     '$SOURCE_ROOT/ppapi/cpp/resource.h',
     '$SOURCE_ROOT/ppapi/cpp/scriptable_object.h',
-    '$SOURCE_ROOT/ppapi/cpp/scrollbar.h',
     '$SOURCE_ROOT/ppapi/cpp/size.h',
-    '$SOURCE_ROOT/ppapi/cpp/url_loader.h',
-    '$SOURCE_ROOT/ppapi/cpp/url_request_info.h',
-    '$SOURCE_ROOT/ppapi/cpp/url_response_info.h',
-    '$SOURCE_ROOT/ppapi/cpp/url_util.h',
     '$SOURCE_ROOT/ppapi/cpp/var.h',
-    '$SOURCE_ROOT/ppapi/cpp/video_decoder.h',
-    '$SOURCE_ROOT/ppapi/cpp/widget.cc',
-    '$SOURCE_ROOT/ppapi/cpp/widget.h',
+    # Dev interfaces.
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_config_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/audio_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/buffer_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_entry_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/directory_reader_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_chooser_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_io_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_ref_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/file_system_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/font_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/graphics_3d_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/scrollbar_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_loader_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_request_info_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_response_info_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/url_util_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/video_decoder_dev.h',
+    '$SOURCE_ROOT/ppapi/cpp/dev/widget_dev.h',
     # from  ppapi.gyp:ppapi_cpp
     '$SOURCE_ROOT/ppapi/cpp/module_embedder.h'
     ]

src/shared/ppapi_proxy/plugin_core.cc
@@ -61,6 +61,10 @@ static void CallOnMainThread(int32_t delay_in_milliseconds,
   // See how NPN_PluginThreadAsyncCall is implemented in npruntime.
 }
 
+bool IsMainThread() {
+  NACL_UNIMPLEMENTED();
+}
+
 }  // namespace
 
 const void* PluginCore::GetInterface() {
@@ -70,7 +74,8 @@ const void* PluginCore::GetInterface() {
     MemAlloc,
     MemFree,
     GetTime,
-    CallOnMainThread
+    CallOnMainThread,
+    IsMainThread
   };
   return reinterpret_cast<const void*>(&intf);
 }

src/shared/ppapi_proxy/plugin_instance.cc
@@ -46,13 +46,14 @@ static bool IsFullFrameThunk(PP_Instance instance) {
   return GetInstancePointer(instance)->IsFullFrame();
 }
 
-static bool SetCursorThunk(PP_Instance instance,
-                           PP_CursorType type,
-                           PP_Resource custom_image,
-                           const PP_Point* hot_spot) {
-  DebugPrintf("PluginInstance::SetCursor: instance=%" NACL_PRIx64"\n",
+PP_Var ExecuteScript(PP_Instance instance, PP_Var script,  PP_Var* exception) {
+  UNREFERENCED_PARAMETER(script);
+  UNREFERENCED_PARAMETER(exception);
+  DebugPrintf("PluginInstance::ExecuteScript: instance=%"
+              NACL_PRIx64 "\n",
               instance);
-  return GetInstancePointer(instance)->SetCursor(type, custom_image, hot_spot);
+  NACL_UNIMPLEMENTED();
+  return PP_MakeVoid();
 }
 
 }  // namespace
@@ -63,7 +64,7 @@ const PPB_Instance* PluginInstance::GetInterface() {
     GetOwnerElementObjectThunk,
     BindGraphicsDeviceContextThunk,
     IsFullFrameThunk,
-    SetCursorThunk
+    ExecuteScript
   };
   return &intf;
 }

src/shared/ppapi_proxy/plugin_instance.h
@@ -22,9 +22,6 @@ class PluginInstance {
   virtual PP_Var GetOwnerElementObject() = 0;
   virtual bool BindGraphicsDeviceContext(PP_Resource device) = 0;
   virtual bool IsFullFrame() = 0;
-  virtual bool SetCursor(PP_CursorType type,
-                         PP_Resource custom_image,
-                         const PP_Point* hot_spot) = 0;
 
   static const PPB_Instance* GetInterface();
 

tests/fake_browser_ppapi/fake_core.cc
@@ -56,6 +56,10 @@ static void CallOnMainThread(int32_t delay_in_milliseconds,
   NACL_UNIMPLEMENTED();
 }
 
+bool IsMainThread() {
+  NACL_UNIMPLEMENTED();
+}
+
 }  // namespace
 
 namespace fake_browser_ppapi {
@@ -67,7 +71,8 @@ const PPB_Core* Core::GetInterface() {
     MemAlloc,
     MemFree,
     GetTime,
-    CallOnMainThread
+    CallOnMainThread,
+    IsMainThread
   };
   return &intf;
 }

commit 1cf65fb21f0b9b1435c62c9f19a2cb7eed1e9e95

Update PPAPI DEPS to fix compile error when building against nacl-glibc
Fixes breakage introduced in r3222.

The error was:
ppapi/cpp/var.cc:316: error: format '%ld' expects type 'long int', but
argument 4 has type 'int32_t' [-Wformat]

BUG=none
TEST=python build.py -b

Review URL: http://codereview.chromium.org/3323013

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3229 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

DEPS
@@ -14,7 +14,7 @@ vars = {
   "tools_rev": "3082",
   "python_rev": "22967",
   "wix_rev": "153",
-  "ppapi_rev": "235",
+  "ppapi_rev": "236",
   "lcov_rev": "54822",
 }
 