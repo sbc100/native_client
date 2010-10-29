sbc100/native_client

commit a7374249da469228c5c6684c7e15ad020a190643

Add module initializer/finalizer for SRPC.  This is necessary to allow a single point to compute the maximum sendmsg size.  Remove SRPC main and relatives. BUG=http://code.google.com/p/nativeclient/issues/detail?id=863 TEST=all the srpc tests are affected by this change.
Review URL: http://codereview.chromium.org/4191001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3586 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/shared/srpc/nacl.scons
@@ -13,6 +13,7 @@ env.Append(CCFLAGS=['-Wno-strict-aliasing'])
 trusted_untrusted_shared = [
     'imc_buffer.c',
     'invoke.c',
+    'module_init_fini.c',
     'nacl_srpc.c',
     'rpc_serialize.c',
     'rpc_service.c',
@@ -22,9 +23,8 @@ trusted_untrusted_shared = [
 
 # NOTE: these files maybe candidates for a separate library
 untrusted_only = [
-    'accept_loop.c',
-    'main.c',
-    'rpc_main.c',
+    'accept.c',
+    'accept_threaded.c',
     'rpc_universal.c']
 
 env.ComponentLibrary('libsrpc',

tests/srpc_without_pthread/nacl.scons
@@ -13,6 +13,7 @@ node = env.CommandSelLdrTestNacl(
     command=[env.File('basic_srpc.nexe')],
     stdin=env.File('basic_srpc_test.stdin'),
     stdout_golden=env.File('basic_srpc_test.stdout'),
+    osenv='NACL_SRPC_STANDALONE=1',
     )
 
 env.AddNodeToTestSuite(node, ['small_tests'], 'run_basic_srpc_test')

src/shared/npruntime/main.cc
@@ -4,52 +4,20 @@
  * found in the LICENSE file.
  */
 
-/*
- * NaCl NPAPI rpc service main loop which starts an interpreter loop
- * for manual rpc invocation.
- * Note: This is a copy of that found in srpc/main.c, but without using
- *       a weak symbol.  This is justified because NPAPI plugins do not
- *       define their own main.
- */
-
-#include <stdlib.h>
-#include <sys/nacl_syscalls.h>
-#include <new>
-
 #include "native_client/src/shared/srpc/nacl_srpc.h"
 #include "gen/native_client/src/shared/npruntime/npnavigator_rpc.h"
 
-static NaClSrpcError Interpreter(NaClSrpcService* service,
-                                 NaClSrpcChannel* channel,
-                                 uint32_t rpc_number,
-                                 NaClSrpcArg** ins,
-                                 NaClSrpcArg** outs) {
-  return (*NaClSrpcServiceMethod(service, rpc_number))(NULL, ins, outs);
-}
-
-// The main definition here is almost identical to that found in
-// src/untrusted/srpc/main.cc.  The definition is required to ensure that
-// the plugin RPC definitions are resolved from the library.
+// NPAPI modules are actually "hosted" by the npruntime library, because
+// they need an SRPC message processing loop to invoke the stubs.
 
 int main(int argc, char* argv[]) {
-  // NOTE: standalone mode happens when a nacl_module is run directly
-  // via sel_ldr not embedded using sel_universal or a plugin.
-  if (NaClSrpcIsStandalone()) {
-    // Build the service.
-    NaClSrpcService* service = reinterpret_cast<NaClSrpcService*>(
-        calloc(1, sizeof(*service)));
-    if (NULL == service) {
-      return 1;
-    }
-    if (!NaClSrpcServiceHandlerCtor(service, NPNavigatorRpcs::srpc_methods)) {
-      free(service);
-      return 1;
-    }
-    // The message processing loop used if sel_ldr is used by itself.
-    NaClSrpcCommandLoop(service, NULL, Interpreter, kNaClSrpcInvalidImcDesc);
+  if (!NaClSrpcModuleInit()) {
+    return 1;
   }
-  // If sel_ldr is invoked from sel_universal or the plugin, the message
-  // processing loop is invoked from start.
+  if (!NaClSrpcAcceptClientConnection(NPNavigatorRpcs::srpc_methods)) {
+    return 1;
+  }
+  NaClSrpcModuleFini();
 
   return 0;
 }

src/shared/ppapi_proxy/plugin_main.cc
@@ -4,54 +4,20 @@
  * found in the LICENSE file.
  */
 
-/*
- * NaCl NPAPI rpc service main loop which starts an interpreter loop
- * for manual rpc invocation.
- * Note: This is a copy of that found in srpc/main.c, but without using
- *       a weak symbol.  This is justified because NPAPI plugins do not
- *       define their own main.
- */
-
-#include <stdlib.h>
-#include <sys/nacl_syscalls.h>
-#include <new>
-
 #include "native_client/src/shared/srpc/nacl_srpc.h"
 #include "gen/native_client/src/shared/ppapi_proxy/ppp_rpc.h"
 
-static NaClSrpcError Interpreter(NaClSrpcService* service,
-                                 NaClSrpcChannel* channel,
-                                 uint32_t rpc_number,
-                                 NaClSrpcArg** ins,
-                                 NaClSrpcArg** outs) {
-  return (*NaClSrpcServiceMethod(service, rpc_number))(NULL, ins, outs);
-}
-
-// The main definition here is almost identical to that found in
-// src/untrusted/srpc/main.cc.  The definition is required to ensure that
-// the plugin RPC definitions are resolved from the library.
+// PPAPI plugins are actually "hosted" by ppruntime.  This is because the
+// library needs to start an SRPC loop to dispatch to the stubs.
 
 int main(int argc, char* argv[]) {
-  const int stand_alone = NaClSrpcIsStandalone();
-  // NOTE: stand_alone mode happens when a nacl_module is run directly
-  // via sel_ldr not using sel_universal or a plugin
-  if (stand_alone) {
-    // Build the service.
-    NaClSrpcService* service = reinterpret_cast<NaClSrpcService*>(
-        calloc(1, sizeof(*service)));
-    if (NULL == service) {
-      return 1;
-    }
-    if (!NaClSrpcServiceHandlerCtor(service, PppRpcs::srpc_methods)) {
-      free(service);
-      return 1;
-    }
-    printf("service set up.\n");
-    // The message processing loop used if sel_ldr is used by itself.
-    NaClSrpcCommandLoop(service, NULL, Interpreter, kNaClSrpcInvalidImcDesc);
+  if (!NaClSrpcModuleInit()) {
+    return 1;
   }
-  // If sel_ldr is invoked from sel_universal or the plugin, the message
-  // processing loop is invoked from start.
+  if (!NaClSrpcAcceptClientConnection(PppRpcs::srpc_methods)) {
+    return 1;
+  }
+  NaClSrpcModuleFini();
 
   return 0;
 }

src/shared/srpc/accept.c
@@ -12,23 +12,23 @@
 #define BOUND_SOCKET 3
 
 
-int NaClSrpcMain(const struct NaClSrpcHandlerDesc *methods) {
+int NaClSrpcAcceptClientConnection(const struct NaClSrpcHandlerDesc *methods) {
+  int sock_fd = -1;
+  int result = 1;
+
   if (NaClSrpcIsStandalone()) {
     return NaClSrpcCommandLoopMain(methods);
-  } else {
-    int sock_fd;
-    int result = 0;
-
-    sock_fd = imc_accept(BOUND_SOCKET);
-    if (sock_fd == -1) {
-      return 1;
-    }
-    if (!NaClSrpcServerLoop(sock_fd, methods, NULL)) {
-      result = 1;
-    }
-    if (close(sock_fd) != 0) {
-      result = 1;
-    }
-    return result;
   }
+
+  sock_fd = imc_accept(BOUND_SOCKET);
+  if (sock_fd == -1) {
+    return 0;
+  }
+  if (!NaClSrpcServerLoop(sock_fd, methods, NULL)) {
+    result = 0;
+  }
+  if (close(sock_fd) != 0) {
+    result = 0;
+  }
+  return result;
 }

src/shared/srpc/accept_loop.c
@@ -1,197 +1,0 @@
-/*
- * Copyright (c) 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-
-#include <pthread.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "native_client/src/shared/srpc/nacl_srpc.h"
-#include "native_client/src/shared/srpc/nacl_srpc_internal.h"
-#include <sys/nacl_syscalls.h>
-
-#define BOUND_SOCKET  3
-
-/* TODO: This file seems to have relative includes and should probably
-   not live in shared/   */
-
-/*
- * If any NACL_SRPC_METHOD declarations are present and the global
- * method table is non-empty (with the exception of __shutdown, which
- * is always declared below), we do implicit SRPC startup, which is
- * triggered by the NaCl/SRPC-specific crt1.o and happens before
- * main().
- *
- * Because this is implicit and magic, it is deprecated.  New code
- * should use NaClSrpcMain() instead.  TODO(mseaborn): Remove once
- * there are no users of this.
- */
-static int ShouldDoImplicitSrpcSetup() {
-  return !(__kNaClSrpcHandlers[0].entry_fmt == NULL ||
-           (strcmp(__kNaClSrpcHandlers[0].entry_fmt, "__shutdown::") == 0 &&
-            __kNaClSrpcHandlers[1].entry_fmt == NULL));
-}
-
-struct worker_state {
-  int d;
-  const struct NaClSrpcHandlerDesc *methods;
-  /*
-   * TODO(mseaborn): Remove is_privileged.  Now that the plugin
-   * creates only one connection, this concept is unused.
-   */
-  int is_privileged;
-};
-
-static pthread_mutex_t shutdown_wait_mu = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t shutdown_wait_cv = PTHREAD_COND_INITIALIZER;
-static int shutdown_done = 0;
-
-/*
- * This is weak symbol and can be implemented in other places.
- * posix_over_srpc library implements it since some internal actions
- * of libposix_over_srpc should be performed when execution reaches __srpc_wait.
- */
-extern void __srpc_wait_hook() __attribute__((weak));
-
-void __srpc_wait() {
-  if (__srpc_wait_hook) {
-    __srpc_wait_hook();
-  }
-  if (ShouldDoImplicitSrpcSetup()) {
-    if (!NaClSrpcIsStandalone()) {
-      pthread_mutex_lock(&shutdown_wait_mu);
-      while (!shutdown_done) {
-        pthread_cond_wait(&shutdown_wait_cv, &shutdown_wait_mu);
-      }
-      pthread_mutex_unlock(&shutdown_wait_mu);
-    }
-  }
-}
-
-static void mark_shutdown_done() {
-  pthread_mutex_lock(&shutdown_wait_mu);
-  shutdown_done = 1;
-  pthread_cond_broadcast(&shutdown_wait_cv);
-  pthread_mutex_unlock(&shutdown_wait_mu);
-}
-
-/**
- * Handle a shutdown request from the untrusted command channel.  We should
- * allow user code to override this to do exit processing -- for now,
- * user code will have to use onexit or atexit to do cleanup, which is
- * suboptimal in a multithreaded environment.
- */
-static NaClSrpcError srpc_shutdown_request(NaClSrpcChannel* channel,
-                                           NaClSrpcArg **in_args,
-                                           NaClSrpcArg **out_arg) {
-  struct worker_state *state =
-      (struct worker_state *) channel->server_instance_data;
-
-  if (state->is_privileged) {
-    /*
-     * do onexit/atexit processing, then exit_group.  really should do
-     * something sane wrt threads, but we do not implement thread
-     * cancellation (as yet?).
-     */
-    mark_shutdown_done();
-    exit(0);
-  }
-  return NACL_SRPC_RESULT_BREAK;
-}
-
-/**
- * Export the shutdown method on the main rpc service loop.  Use __shutdown
- * to avoid name collisions.
- * TODO(mseaborn): Remove __shutdown because it is unused.
- */
-NACL_SRPC_METHOD("__shutdown::", srpc_shutdown_request);
-
-/**
- * Basic SRPC worker thread: run the NaClSrpcServerLoop.
- */
-static void *srpc_worker(void *arg) {
-  struct worker_state *state = (struct worker_state *) arg;
-
-  NaClSrpcServerLoop(state->d, state->methods, NULL);
-
-  (void) close(state->d);
-  if (state->is_privileged) {
-    mark_shutdown_done();
-    _exit(0);
-  }
-  free(arg);
-  return 0;
-}
-
-/**
- * Acceptor loop: accept client connections, and for each, spawn a
- * worker thread that invokes NaClSrpcServerLoop.
- */
-static void *srpc_default_acceptor(void *arg) {
-  struct NaClSrpcHandlerDesc *methods = arg;
-  int       first = 1;
-  int       d;
-
-  while (-1 != (d = imc_accept(BOUND_SOCKET))) {
-    struct worker_state *state = malloc(sizeof *state);
-    pthread_t           worker_tid;
-
-    if (NULL == state) {
-      /*
-       * shed load; the client can come back later when we have more
-       * memory.
-       */
-      (void) close(d);
-      continue;
-    }
-    state->d = d;
-    state->methods = methods;
-    state->is_privileged = first;
-    /* worker thread is responsible for state and d. */
-    pthread_create(&worker_tid, NULL, srpc_worker, state);
-    first = 0;
-  }
-  return NULL;
-}
-
-/**
- * Internal SRPC initialization.
- *
- * First we check to see if we are running embedded in the browser. If
- * not, we simply return, as there will be no SRPC connections. If
- * embedded we spawn a thread which is our main accept loop. The accept
- * loop spawns worker threads on a per-client-connection basis.
- * (We could use a thread pool, but do not at this point.)  The
- * worker threads just handle RPC requests using NaClSrpcServerLoop().
- * The first worker thread is "privileged", in that it is responsible
- * for shutting down the NaCl app, and we expect that this first
- * connection comes from the browser plugin.
- */
-void __srpc_init() {
-  pthread_t acceptor_tid;
-  static int init_done = 0;
-
-  /*
-   * In context of a browser __srpc_init() is invoked after _init() to make sure
-   * all global constructors have finished when we accept the first RPC. Other
-   * contexts may require SRPC be initialized before global constructors.
-   * Subsequent calls to __srpc_init() must execute as a noop.
-   */
-  if (init_done) {
-    return;
-  }
-  init_done = 1;
-
-  if (ShouldDoImplicitSrpcSetup()) {
-    if (!NaClSrpcIsStandalone()) {
-      /*
-       * Start the acceptor thread.
-       */
-      pthread_create(&acceptor_tid, NULL,
-                     srpc_default_acceptor, (void *) __kNaClSrpcHandlers);
-      pthread_detach(acceptor_tid);
-    }
-  }
-}

src/shared/srpc/accept_threaded.c
@@ -1,0 +1,66 @@
+/*
+ * Copyright (c) 2010 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/nacl_syscalls.h>
+#include <unistd.h>
+
+#include "native_client/src/shared/srpc/nacl_srpc.h"
+
+#define BOUND_SOCKET 3
+
+/* Hack: interaction with AV library.  Wait for AV startup RPC. */
+void __attribute__ ((weak))  __av_wait() { }
+
+struct WorkerData {
+  int sock_fd;
+  const struct NaClSrpcHandlerDesc *methods;
+};
+
+static void *Worker(void *arg) {
+  struct WorkerData *worker_data = (struct WorkerData *) arg;
+  NaClSrpcServerLoop(worker_data->sock_fd, worker_data->methods, NULL);
+  close(worker_data->sock_fd);
+  free(worker_data);
+  return NULL;
+}
+
+int NaClSrpcAcceptClientOnThread(const struct NaClSrpcHandlerDesc* methods) {
+  int sock_fd = -1;
+  int result = 0;
+  struct WorkerData *worker_data = NULL;
+  pthread_t worker_tid;
+
+  if (NaClSrpcIsStandalone()) {
+    return NaClSrpcCommandLoopMain(methods);
+  }
+
+  sock_fd = imc_accept(BOUND_SOCKET);
+  if (sock_fd == -1) {
+    goto done;
+  }
+  worker_data = (struct WorkerData*) malloc(sizeof *worker_data);
+  if (worker_data == NULL) {
+    goto done;
+  }
+  worker_data->sock_fd = sock_fd;
+  worker_data->methods = methods;
+  if (pthread_create(&worker_tid, NULL, Worker, (void *) worker_data) != 0) {
+    goto done;
+  }
+  /* On successful thread start the worker takes ownership. */
+  worker_data = NULL;
+  sock_fd = -1;
+  __av_wait();
+  result = 1;
+ done:
+  free(worker_data);
+  if (sock_fd != -1) {
+    close(sock_fd);
+  }
+  return result;
+}

src/shared/srpc/build.scons
@@ -10,6 +10,7 @@ Import('env')
 trusted_untrusted_shared = [
     'imc_buffer.c',
     'invoke.c',
+    'module_init_fini.c',
     'nacl_srpc.c',
     'rpc_serialize.c',
     'rpc_service.c',

commit 3a53dd012be556e88c1e2070c9159e7661480e57

Disable building libav-using tests when using nacl-glibc
This fixes a failure on Buildbot introduced by r3586.

r3586 meant that libav-using programs now need to use
__kNaClSrpcHandlers, but this is not available with nacl-glibc.
So disable building these tests.

This change partially reverts r3589 and r3590, which disabled running
these tests but not building them.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=863
TEST=python build.py scons_compile_tests scons_tests -b

Review URL: http://codereview.chromium.org/4216002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3595 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -303,13 +303,6 @@ nacl_glibc_skiplist = [
     # Syscall wrappers not implemented yet.
     'run_sysbasic_test',
     'run_sysbrk_test',
-    # Depends on libav, and hence __kNaClSrpcHandlers being defined in crt1.o
-    'run_earth',
-    'earth_browser_test',
-    'run_life',
-    'run_mandel',
-    'run_mandel_nav',
-    'run_voronoi',
     # Needs further investigation.
     'sdk_minimal_test',
     # TODO(elijahtaylor) add apropriate syscall hooks for glibc

tests/earth/nacl.scons
@@ -5,6 +5,13 @@
 
 Import('env')
 
+if env.Bit('nacl_glibc'):
+  # Programs using libav now use __kNaClSrpcHandlers, which does not
+  # work when linking against nacl-glibc.
+  # TODO(mseaborn): Remove __kNaClSrpcHandlers fully and re-enable this.
+  # See http://code.google.com/p/nativeclient/issues/detail?id=863
+  Return()
+
 nexe = env.ComponentProgram('earth.nexe', 'earth.cc',
                             EXTRA_LIBS=['av', 'srpc', 'm', 'pthread'])
 

tests/life/nacl.scons
@@ -5,6 +5,13 @@
 
 Import('env')
 
+if env.Bit('nacl_glibc'):
+  # Programs using libav now use __kNaClSrpcHandlers, which does not
+  # work when linking against nacl-glibc.
+  # TODO(mseaborn): Remove __kNaClSrpcHandlers fully and re-enable this.
+  # See http://code.google.com/p/nativeclient/issues/detail?id=863
+  Return()
+
 nexe = env.ComponentProgram('life.nexe',
                             ['life.cc',
                              ],

tests/mandel_nav/nacl.scons
@@ -5,6 +5,13 @@
 
 Import('env')
 
+if env.Bit('nacl_glibc'):
+  # Programs using libav now use __kNaClSrpcHandlers, which does not
+  # work when linking against nacl-glibc.
+  # TODO(mseaborn): Remove __kNaClSrpcHandlers fully and re-enable this.
+  # See http://code.google.com/p/nativeclient/issues/detail?id=863
+  Return()
+
 mandel_nav_files = [
     'mandel_nav.html',
     'mandel_nav.js',

tests/voronoi/nacl.scons
@@ -5,6 +5,13 @@
 
 Import('env')
 
+if env.Bit('nacl_glibc'):
+  # Programs using libav now use __kNaClSrpcHandlers, which does not
+  # work when linking against nacl-glibc.
+  # TODO(mseaborn): Remove __kNaClSrpcHandlers fully and re-enable this.
+  # See http://code.google.com/p/nativeclient/issues/detail?id=863
+  Return()
+
 ######################################################################
 # Smoke Test
 ######################################################################