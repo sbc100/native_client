sbc100/native_client

commit 37e74cff0b5ef3f7bf7da088f6770624e012d9d1

Scons: Declare missing dependency of sel_universal-based tests on sel_ldr
Also factor out the duplicated check that disables these tests on ARM/QEMU.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1341
TEST=rm -r scons-out/dbg-linux-x86-32/staging; ./scons --mode=dbg-linux,nacl run_srpc_basic_test

Review URL: http://codereview.chromium.org/6339009

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4218 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -1081,6 +1081,21 @@ def CommandGdbTestNacl(env, name, command,
 pre_base_env.AddMethod(CommandGdbTestNacl)
 
 
+def SelUniversalTest(env, name, command, **kwargs):
+  # sel_universal does not know how to invoke sel_ldr via QEMU, so it
+  # does not work when testing the ARM build on another architecture.
+  # See http://code.google.com/p/nativeclient/issues/detail?id=1300
+  if env.Bit('target_arm') and env.UsingEmulator():
+    return []
+  node = CommandSelLdrTestNacl(env, name, command, loader='sel_universal',
+                               **kwargs)
+  # sel_universal locates sel_ldr via /proc/self/exe on Linux.
+  env.Depends(node, GetSelLdr(env))
+  return node
+
+pre_base_env.AddMethod(SelUniversalTest)
+
+
 # ----------------------------------------------------------
 def AddToStringifiedList(lst, additions):
   if not lst:

src/trusted/service_runtime/build.scons
@@ -596,9 +596,6 @@ if ((not env.Bit('windows') or not env.Bit('coverage_enabled')) and
 # ./scons platform=x86-32
 # cp scons-out/nacl-x86-32/staging/{mandel.nexe,fib_*} \
 #     src/trusted/service_runtime/testdata/x86_32/
-# NOTE: sel_universal launches sel_ldr internally and we do not have a
-# good hook to sneak in an emulator yet
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
 
 # TODO: Create integration test nexes for arm.
 INTEGRATION_TESTS_X86 = [ 'mandel',
@@ -612,17 +609,15 @@ def AddIntegrationTest(test, location):
   if not test or not location:
     return;
 
-  node = env.CommandSelLdrTestNacl(
+  node = env.SelUniversalTest(
       test + '_canned.out',
-      loader='sel_universal',
       command = [env.File(arch_testdata_dir + '/' + test + '.nexe')],
       stdin = location + '.stdin',
       stdout_golden = location + '.stdout',
       )
   env.AddNodeToTestSuite(node,
                          ['medium_tests'],
-                         'run_%s_integration_test' % test,
-                         is_broken=is_broken)
+                         'run_%s_integration_test' % test)
 
 if env.Bit('target_x86'):
   RE_HELLO = '^(Hello, World!)$'

tests/fib/nacl.scons
@@ -5,37 +5,30 @@
 
 Import('env')
 
-# http://code.google.com/p/nativeclient/issues/detail?id=1300
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
-
 env.ComponentProgram('fib_array.nexe',
                      'fib_array.c',
                      EXTRA_LIBS=['srpc', 'google_nacl_imc_c', 'pthread'])
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'fib_array_test.out',
-    loader='sel_universal',
     command=[env.File('fib_array.nexe')],
     stdin=env.File('fib_array_test.stdin'),
     stdout_golden=env.File('fib_array_test.stdout')
     )
 env.AddNodeToTestSuite(node,
                        ['small_tests', 'sel_ldr_tests'],
-                       'run_fib_array_test',
-                       is_broken=is_broken)
+                       'run_fib_array_test')
 
 
 
 env.ComponentProgram('fib_scalar.nexe',
                      'fib_scalar.c',
                      EXTRA_LIBS=['srpc', 'google_nacl_imc_c', 'pthread'])
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'fib_scalar_test.out',
-    loader='sel_universal',
     command=[env.File('fib_scalar.nexe')],
     stdin=env.File('fib_scalar_test.stdin'),
     stdout_golden=env.File('fib_scalar_test.stdout')
     )
 env.AddNodeToTestSuite(node,
                        ['small_tests', 'sel_ldr_tests'],
-                       'run_fib_scalar_test',
-                       is_broken=is_broken)
+                       'run_fib_scalar_test')

tests/mandel/nacl.scons
@@ -17,17 +17,12 @@ env.ComponentProgram('mandel.nexe', 'mandel.c',
 
 env.Publish('mandel.nexe', 'run', [])
 
-# http://code.google.com/p/nativeclient/issues/detail?id=1300
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
-
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'mandel_test.out',
-    loader='sel_universal',
     command=[env.File('mandel.nexe')],
     stdin=env.File('test.stdin'),
     stdout_golden=env.File('test.stdout')
     )
 env.AddNodeToTestSuite(node,
                        ['small_tests', 'sel_ldr_tests'],
-                       'run_mandel',
-                       is_broken=is_broken)
+                       'run_mandel')

tests/ppapi_example_2d/nacl.scons
@@ -22,9 +22,8 @@ ppapi_example_2d_nexe = env.ComponentProgram('ppapi_example_2d.nexe',
 env.Publish('ppapi_example_2d.nexe', 'run',
             ['ppapi_example_2d.html' ])
 
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'ppapi_example_2d.out',
-    loader='sel_universal',
     command=[env.File('ppapi_example_2d.nexe')],
     stdin=env.File('ppapi_example_2d.stdin'),
     stdout_golden=env.File('ppapi_example_2d.stdout'),
@@ -34,10 +33,7 @@ node = env.CommandSelLdrTestNacl(
     )
 
 # we the sysv shm feature in sel_universal which is only available on linux
-trusted_env = env.get('TRUSTED_ENV')
-is_broken = (env.UsingEmulator() or
-             (not trusted_env) or
-             (not trusted_env.Bit('linux')))
+is_broken = not env.Bit('host_linux')
 env.AddNodeToTestSuite(node,
                        ['small_tests', 'sel_ldr_tests'],
                        'run_ppapi_example_2d_test',

tests/srpc/nacl.scons
@@ -98,38 +98,27 @@ env.ComponentProgram('srpc_bad_service.nexe', 'srpc_bad_service.c',
 env.Publish('srpc_bad_service.nexe', 'run', [])
 
 
-# NOTE: sel_universal launches sel_ldr internally and we do not have a
-# good hook to sneak in an emulator yet
-# TODO(robertm): research why srpc_basic and sysv_shm do not work on pnacl arm
-# "execv failed, args were: ... execv: Exec format error Open failed"
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
-
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'srpc_basic_test.out',
-    loader='sel_universal',
     command=[env.File('srpc_test.nexe')],
     stdin=env.File('srpc_basic_test.stdin'),
     stdout_golden=env.File('srpc_basic_test.stdout'),
     )
-env.AddNodeToTestSuite(node, ['small_tests'], 'run_srpc_basic_test',
-                       is_broken=is_broken)
+env.AddNodeToTestSuite(node, ['small_tests'], 'run_srpc_basic_test')
 
 trusted_env = env.get('TRUSTED_ENV')
 # NOTE: sel_universal only supports shmem under linux
 if trusted_env and trusted_env.Bit('linux'):
-  node = env.CommandSelLdrTestNacl(
+  node = env.SelUniversalTest(
       'srpc_sysv_shm_test.out',
-      loader='sel_universal',
       command=[env.File('srpc_shm.nexe')],
       stdin=env.File('srpc_sysv_shm_test.stdin'),
       stdout_golden=env.File('srpc_sysv_shm_test.stdout'),
       )
-  env.AddNodeToTestSuite(node, ['small_tests'], 'run_srpc_sysv_shm_test',
-                         is_broken=is_broken)
+  env.AddNodeToTestSuite(node, ['small_tests'], 'run_srpc_sysv_shm_test')
 
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'srpc_bad_service.out',
-    loader='sel_universal',
     command=[env.File('srpc_bad_service.nexe')],
     exit_status='1'
     )

tests/srpc_without_pthread/nacl.scons
@@ -10,18 +10,10 @@ env.Replace(EXTRA_LIBS=['srpc'])
 env.ComponentProgram('basic_srpc.nexe', 'basic_srpc.c')
 
 
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'basic_srpc_test.out',
-    loader='sel_universal',
     command=[env.File('basic_srpc.nexe')],
     stdin=env.File('basic_srpc_test.stdin'),
     stdout_golden=env.File('basic_srpc_test.stdout'),
     )
-
-# http://code.google.com/p/nativeclient/issues/detail?id=1300
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
-
-env.AddNodeToTestSuite(node,
-                       ['small_tests'],
-                       'run_basic_srpc_test',
-                       is_broken=is_broken)
+env.AddNodeToTestSuite(node, ['small_tests'], 'run_basic_srpc_test')

tests/voronoi/nacl.scons
@@ -12,20 +12,13 @@ nexe = env.ComponentProgram('voronoi.nexe',
                             ['voronoi.cc'],
                             EXTRA_LIBS=['av', 'srpc', 'm', 'pthread'])
 
-# http://code.google.com/p/nativeclient/issues/detail?id=1300
-is_broken = env.Bit('target_arm') and env.UsingEmulator()
-
-node = env.CommandSelLdrTestNacl(
+node = env.SelUniversalTest(
     'voronoi_test.out',
-    loader='sel_universal',
     command=[env.File('voronoi.nexe'), '-t', '-d'],
     stdout_golden=env.File('voronoi.stdout'),
     stdin=env.File('voronoi.stdin'),
     )
-env.AddNodeToTestSuite(node,
-                       ['small_tests'],
-                       'run_voronoi',
-                       is_broken=is_broken)
+env.AddNodeToTestSuite(node, ['small_tests'], 'run_voronoi')
 
 env.Publish('voronoi.nexe', 'run', ['voronoi.html',])
 

commit 6017c6f7f377191b291abdd99695e049ff408edb

Scons: Fix SelUniversalTest() to work with built_elsewhere=1
This should fix Buildbot breakage introduced by r4218.

The dependency on sel_ldr does not work when using built_elsewhere=1,
which is used in the ARM (non-emulated) and Windows Atom bots.  The
bots attempt to rebuild sel_ldr in the small_tests phase, and this
fails.  On Windows, it fails because "cl" is not present.  On ARM, it
looks like the resulting executable (presumably built using the
system's gcc) is bad in some way.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1341
TEST=as for r4218

Review URL: http://codereview.chromium.org/6277019

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4221 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -1098,7 +1098,8 @@ def SelUniversalTest(env, name, command, **kwargs):
   node = CommandSelLdrTestNacl(env, name, command, loader='sel_universal',
                                **kwargs)
   # sel_universal locates sel_ldr via /proc/self/exe on Linux.
-  env.Depends(node, GetSelLdr(env))
+  if not env.Bit('built_elsewhere'):
+    env.Depends(node, GetSelLdr(env))
   return node
 
 pre_base_env.AddMethod(SelUniversalTest)