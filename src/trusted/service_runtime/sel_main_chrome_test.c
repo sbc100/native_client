sbc100/native_client

commit 9efd303933097df6aafcf275e627ef7320ac8fc3

Provide a more extensible startup interface to Chromium
Rather than providing NaClMainForChromium() plus various global
setters, we use a non-global struct of arguments.  This will make it
easier to add and remove arguments in the future.

This is in preparation for adding an "enable_exception_handling"
argument.

Following the recent cleanup of SelLdrLauncher, the array of handles
is replaced with a single handle (imc_bootstrap_handle).

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2651
TEST=built Chromium locally

Review URL: https://chromiumcodereview.appspot.com/9695061

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8019 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/shared/imc/nacl_imc_unistd.cc
@@ -116,9 +116,16 @@ static int TryShmOrTempOpen(size_t length, const char* prefix, bool use_temp) {
 }
 
 static CreateMemoryObjectFunc g_create_memory_object_func = NULL;
+// This is the usable-from-C variant that takes an int rather than a bool.
+// TODO(mseaborn): Switch to having only the int variant.
+static NaClCreateMemoryObjectFunc g_create_memory_object_func_c = NULL;
 
 void SetCreateMemoryObjectFunc(CreateMemoryObjectFunc func) {
   g_create_memory_object_func = func;
+}
+
+void NaClSetCreateMemoryObjectFunc(NaClCreateMemoryObjectFunc func) {
+  g_create_memory_object_func_c = func;
 }
 
 Handle CreateMemoryObject(size_t length, bool executable) {
@@ -129,6 +136,12 @@ Handle CreateMemoryObject(size_t length, bool executable) {
 
   if (g_create_memory_object_func != NULL) {
     fd = g_create_memory_object_func(length, executable);
+    if (fd >= 0)
+      return fd;
+  }
+
+  if (g_create_memory_object_func_c != NULL) {
+    fd = g_create_memory_object_func_c(length, executable);
     if (fd >= 0)
       return fd;
   }

src/trusted/service_runtime/sel_main_chrome.c
@@ -78,7 +78,6 @@ static void NaClLoadIrt(struct NaClApp *nap) {
   (*NACL_VTBL(Gio, gio_desc)->Dtor)(gio_desc);
 }
 
-/* TODO(ncbray) consolidate Chrome's sel_ldr setup to a single function. */
 void NaClSetValidationCache(struct NaClValidationCache *cache) {
   g_validation_cache = cache;
 }
@@ -309,4 +308,24 @@ void NaClMainForChromium(int handle_count, const NaClHandle *handles,
   NaClAllModulesFini();
 
   NaClExit(ret_code);
+}
+
+struct NaClChromeMainArgs *NaClChromeMainArgsInit(void) {
+  struct NaClChromeMainArgs *args = malloc(sizeof(*args));
+  if (args == NULL)
+    return NULL;
+  args->imc_bootstrap_handle = NACL_INVALID_HANDLE;
+  args->irt_fd = -1;
+  args->enable_debug_stub = 0;
+  args->create_memory_object_func = NULL;
+  args->validation_cache = NULL;
+  return args;
+}
+
+void NaClChromeMainStart(struct NaClChromeMainArgs *args) {
+  NaClSetIrtFileDesc(args->irt_fd);
+  if (args->create_memory_object_func != NULL)
+    NaClSetCreateMemoryObjectFunc(args->create_memory_object_func);
+  NaClSetValidationCache(args->validation_cache);
+  NaClMainForChromium(1, &args->imc_bootstrap_handle, args->enable_debug_stub);
 }

src/shared/imc/nacl_imc_c.h
@@ -215,6 +215,19 @@ int NaClReceiveDatagram(NaClHandle socket, NaClMessageHeader* message,
                         int flags);
 
 /*
+ * Type of function supplied to NaClSetCreateMemoryObjectFunc().  See
+ * nacl::CreateMemoryObjectFunc.
+ */
+typedef NaClHandle (*NaClCreateMemoryObjectFunc)(size_t length, int executable);
+
+/*
+ * This allows an alternative implementation of NaClCreateMemoryObject()
+ * to be provided that works in an outer sandbox. See
+ * nacl::SetCreateMemoryObjectFunc().
+ */
+void NaClSetCreateMemoryObjectFunc(NaClCreateMemoryObjectFunc func);
+
+/*
  * Creates a memory object of length bytes.
  *
  * NaClCreateMemoryObject() returns a handle of the newly created

src/trusted/service_runtime/sel_main_chrome.h
@@ -14,6 +14,47 @@ EXTERN_C_BEGIN
 
 struct NaClValidationCache;
 
+
+struct NaClChromeMainArgs {
+  /*
+   * Handle for bootstrapping a NaCl IMC connection to the trusted
+   * PPAPI plugin.  Required.
+   */
+  NaClHandle imc_bootstrap_handle;
+
+  /*
+   * File descriptor for the NaCl integrated runtime (IRT) library.
+   * Note that this is a file descriptor even on Windows (where file
+   * descriptors are emulated by the C runtime library).  Required.
+   */
+  int irt_fd;
+
+  /* Whether to enable NaCl's built-in GDB RSP debug stub.  Boolean. */
+  int enable_debug_stub;
+
+  /*
+   * Callback to use for creating shared memory objects.  Optional;
+   * may be NULL.
+   */
+  NaClCreateMemoryObjectFunc create_memory_object_func;
+
+  /* Cache for NaCl validation judgements.  Optional; may be NULL. */
+  struct NaClValidationCache *validation_cache;
+};
+
+/* Create a new args struct containing default values. */
+struct NaClChromeMainArgs *NaClChromeMainArgsCreate(void);
+
+/* Launch NaCl. */
+void NaClChromeMainStart(struct NaClChromeMainArgs *args);
+
+
+/*
+ * TODO(mseaborn): The functions below are the old interface used by
+ * Chromium.  Once Chromium is switched to the new interface (above),
+ * remove the old interface.
+ */
+
 /*
  * Register the integrated runtime (IRT) library file for use by
  * NaClMainForChromium().  This takes a file descriptor, even on
@@ -26,6 +67,7 @@ void NaClSetValidationCache(struct NaClValidationCache *cache);
 
 void NaClMainForChromium(int handle_count, const NaClHandle *handles,
                          int debug);
+
 
 EXTERN_C_END
 

commit 1a6b659b79e221e5a770b8731c06e443c9794687

Fix linking of sel_main_chrome.c (broken by r8019)
Fix linking on Unix: Make sure that NaClSetCreateMemoryObjectFunc() is
defined outside of "namespace nacl" so that is compiled as 'extern
"C"'.  Move the global variables to the top of the file.  Move
NaClDuplicateNaClHandle() to keep the 'extern "C"' functions together.

Fix linking on Windows: Add an empty NaClSetCreateMemoryObjectFunc()
for Windows.

Fix the definition of NaClChromeMainArgsCreate() to use the correct
name!

Add a test to cover linking of sel_main_chrome.c.

r8019 broke Chromium integration: I had tested building Chromium
locally, on a Mac, but I only tested an earlier version of the change.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2651
TEST=trybots

Review URL: https://chromiumcodereview.appspot.com/9703033

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8027 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/shared/imc/nacl_imc_unistd.cc
@@ -34,12 +34,6 @@
 #include "native_client/src/shared/imc/nacl_imc_c.h"
 #include "native_client/src/shared/platform/nacl_check.h"
 
-// Duplicate a file descriptor.
-NaClHandle NaClDuplicateNaClHandle(NaClHandle handle) {
-  return dup(handle);
-}
-
-namespace nacl {
 
 namespace {
 
@@ -50,7 +44,25 @@ const char kNaClTempPrefixVar[] = "NACL_TMPFS_PREFIX";
 const char kShmTempPrefix[] = "/tmp/google-nacl-shm-";
 const char kShmOpenPrefix[] = "/google-nacl-shm-";
 
+nacl::CreateMemoryObjectFunc g_create_memory_object_func = NULL;
+// This is the usable-from-C variant that takes an int rather than a bool.
+// TODO(mseaborn): Switch to having only the int variant.
+NaClCreateMemoryObjectFunc g_create_memory_object_func_c = NULL;
+
 }  // namespace
+
+
+// Duplicate a file descriptor.
+NaClHandle NaClDuplicateNaClHandle(NaClHandle handle) {
+  return dup(handle);
+}
+
+void NaClSetCreateMemoryObjectFunc(NaClCreateMemoryObjectFunc func) {
+  g_create_memory_object_func_c = func;
+}
+
+
+namespace nacl {
 
 bool WouldBlock() {
   return (errno == EAGAIN) ? true : false;
@@ -115,17 +127,8 @@ static int TryShmOrTempOpen(size_t length, const char* prefix, bool use_temp) {
   }
 }
 
-static CreateMemoryObjectFunc g_create_memory_object_func = NULL;
-// This is the usable-from-C variant that takes an int rather than a bool.
-// TODO(mseaborn): Switch to having only the int variant.
-static NaClCreateMemoryObjectFunc g_create_memory_object_func_c = NULL;
-
 void SetCreateMemoryObjectFunc(CreateMemoryObjectFunc func) {
   g_create_memory_object_func = func;
-}
-
-void NaClSetCreateMemoryObjectFunc(NaClCreateMemoryObjectFunc func) {
-  g_create_memory_object_func_c = func;
 }
 
 Handle CreateMemoryObject(size_t length, bool executable) {

src/shared/imc/win/nacl_shm.cc
@@ -1,7 +1,7 @@
 /*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 
@@ -9,8 +9,15 @@
 
 #include <windows.h>
 #include "native_client/src/shared/imc/nacl_imc.h"
+#include "native_client/src/shared/imc/nacl_imc_c.h"
 #include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/trusted/service_runtime/include/sys/mman.h"
+
+// This function is a no-op on Windows because there is no need to
+// override the Windows definition of NaClCreateMemoryObject(): it
+// already works inside the outer sandbox.
+void NaClSetCreateMemoryObjectFunc(NaClCreateMemoryObjectFunc func) {
+}
 
 namespace nacl {
 

src/trusted/service_runtime/build.scons
@@ -80,8 +80,8 @@ ldr_inputs = [
     'sel_ldr.c',
     'sel_ldr_standard.c',
     'sel_ldr_thread_interface.c',
-    # sel_main_chrome.c is compile-tested here but not otherwise used
-    # in the Scons build.
+    # sel_main_chrome.c is compile-tested and link-tested here but not
+    # otherwise used in the Scons build.
     'sel_main_chrome.c',
     'sel_mem.c',
     'sel_qualify.c',
@@ -950,3 +950,9 @@ if (not env.CrossToolsBuild() and
   env.AddNodeToTestSuite(node, ['medium_tests'],
                          'run_sel_ldr_thread_death_test',
                          is_broken=env.Bit('windows'))
+
+
+# This currently tests linking of sel_main_chrome.c *only*.  We do not
+# expect this program to do anything meaningful when run.
+env.ComponentProgram('sel_main_chrome_test', ['sel_main_chrome_test.c'],
+                     EXTRA_LIBS=sel_ldr_libs)

src/trusted/service_runtime/sel_main_chrome.c
@@ -310,7 +310,7 @@ void NaClMainForChromium(int handle_count, const NaClHandle *handles,
   NaClExit(ret_code);
 }
 
-struct NaClChromeMainArgs *NaClChromeMainArgsInit(void) {
+struct NaClChromeMainArgs *NaClChromeMainArgsCreate(void) {
   struct NaClChromeMainArgs *args = malloc(sizeof(*args));
   if (args == NULL)
     return NULL;

src/trusted/service_runtime/service_runtime.gyp
@@ -302,8 +302,20 @@
           ],
         }],
       ],
+    }, {
+       # This currently tests linking of sel_main_chrome.c *only*.  We
+       # do not expect this program to do anything meaningful when
+       # run.
+      'target_name': 'sel_main_chrome_test',
+      'type': 'executable',
+      'dependencies': [
+        'sel',
+        '<(DEPTH)/native_client/src/trusted/platform_qualify/platform_qualify.gyp:platform_qual_lib',
+      ],
+      'sources': [
+        'sel_main_chrome_test.c',
+      ],
     },
-    # no tests are built here; see service_runtime_test.gyp
   ],
   'conditions': [
     ['OS=="win"', {
@@ -401,7 +413,23 @@
             'sel_main.c',
           ],
         },
-        # TODO(bsy): no tests are built; see build.scons
+        {
+          # This currently tests linking of sel_main_chrome.c *only*.  We
+          # do not expect this program to do anything meaningful when
+          # run.
+          'target_name': 'sel_main_chrome_test64',
+          'type': 'executable',
+          'variables': {
+            'win_target': 'x64',
+          },
+          'dependencies': [
+            'sel64',
+            '<(DEPTH)/native_client/src/trusted/platform_qualify/platform_qualify.gyp:platform_qual_lib64',
+          ],
+          'sources': [
+            'sel_main_chrome_test.c',
+          ],
+        },
       ],
     }],
   ]

src/trusted/service_runtime/sel_main_chrome_test.c
@@ -1,0 +1,18 @@
+/*
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "native_client/src/trusted/service_runtime/sel_main_chrome.h"
+
+
+int main() {
+  /*
+   * This currently tests linking of sel_main_chrome.c *only*.  We do
+   * not expect this program to do anything meaningful when run.
+   */
+  struct NaClChromeMainArgs *args = NaClChromeMainArgsCreate();
+  NaClChromeMainStart(args);
+  return 1;
+}