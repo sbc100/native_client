sbc100/native_client

commit eb6603c369df9fb7471c2060319ab89fa2baebd7

Debug stub: Move initialisation out of sel_ldr_standard.c
The initialisation was only done in sel_ldr_standard.c because the
debug stub wanted to get the argv/env arrays, but this has since been
removed.

Doing the initialisation from sel_main*.c will make it easier to avoid
a circular dependency between the 'sel' and 'debug_stub' libraries.
This dependency would occur when the debug stub starts to use
service_runtime's thread suspension functions.  sel_main.c is not in
the 'sel' library (and sel_main_chrome.c doesn't have to be in 'sel',
although we need a Chromium-side change before we can move it out of
'sel').

This doesn't really complicate sel_main.c/sel_main_chrome.c since they
already needed to pass through a debug_stub_enabled setting.

This change would also make it easier, in the future, to initialise
the debug stub's TCP port before the Mac outer sandbox is enabled.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2758
TEST=run_debug_stub_test

Review URL: https://chromiumcodereview.appspot.com/10580025

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8988 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/sel_main_chrome.c
@@ -28,6 +28,7 @@
 #include "native_client/src/trusted/service_runtime/env_cleanser.h"
 #include "native_client/src/trusted/service_runtime/nacl_app.h"
 #include "native_client/src/trusted/service_runtime/nacl_all_modules.h"
+#include "native_client/src/trusted/service_runtime/nacl_debug_init.h"
 #include "native_client/src/trusted/service_runtime/nacl_signal.h"
 #include "native_client/src/trusted/service_runtime/osx/mach_exception_handler.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
@@ -291,11 +292,6 @@ void NaClChromeMainStart(struct NaClChromeMainArgs *args) {
   /* Load the integrated runtime (IRT) library. */
   NaClLoadIrt(nap, args->irt_fd);
 
-  /*
-   * Enable debugging if requested.
-   */
-  nap->enable_debug_stub = args->enable_debug_stub;
-
   free(args);
   args = NULL;
 
@@ -307,6 +303,11 @@ void NaClChromeMainStart(struct NaClChromeMainArgs *args) {
   if (!NaClAppLaunchServiceThreads(nap)) {
     fprintf(stderr, "Launch service threads failed\n");
     goto done;
+  }
+  if (args->enable_debug_stub) {
+    if (!NaClDebugInit(nap)) {
+      goto done;
+    }
   }
   if (!NaClCreateMainThread(nap, ac, av,
                             NaClEnvCleanserEnvironment(&env_cleanser))) {

src/trusted/service_runtime/sel_ldr.c
@@ -225,7 +225,6 @@ int NaClAppWithSyscallTableCtor(struct NaClApp               *nap,
   nap->data_seg_sel = 0;
 #endif
 
-  nap->enable_debug_stub = 0;
   nap->debug_stub_callbacks = NULL;
   nap->exception_handler = 0;
   if (!NaClMutexCtor(&nap->exception_mu)) {

src/trusted/service_runtime/sel_ldr.h
@@ -357,7 +357,6 @@ struct NaClApp {
   struct NaClFastMutex      desc_mu;
   struct DynArray           desc_tbl;  /* NaClDesc pointers */
 
-  int                       enable_debug_stub;
   const struct NaClDebugCallbacks *debug_stub_callbacks;
   struct NaClMutex          exception_mu;
   uint32_t                  exception_handler;

src/trusted/service_runtime/sel_ldr_standard.c
@@ -36,7 +36,6 @@
 #include "native_client/src/trusted/service_runtime/arch/sel_ldr_arch.h"
 #include "native_client/src/trusted/service_runtime/elf_util.h"
 #include "native_client/src/trusted/service_runtime/nacl_app_thread.h"
-#include "native_client/src/trusted/service_runtime/nacl_debug_init.h"
 #include "native_client/src/trusted/service_runtime/nacl_kern_services.h"
 #include "native_client/src/trusted/service_runtime/nacl_oop_debugger_hooks.h"
 #include "native_client/src/trusted/service_runtime/nacl_switch_to_app.h"
@@ -743,15 +742,6 @@ int NaClCreateMainThread(struct NaClApp     *nap,
   }
   if (NULL == envv_len && 0 != envc) {
     goto cleanup;
-  }
-
-  if (nap->enable_debug_stub) {
-    /*
-     * Enable the debug stub.
-     */
-    if (!NaClDebugInit(nap)) {
-      goto cleanup;
-    }
   }
 
   size = 0;

src/trusted/service_runtime/sel_main.c
@@ -39,6 +39,7 @@
 #include "native_client/src/trusted/service_runtime/include/sys/fcntl.h"
 #include "native_client/src/trusted/service_runtime/nacl_app.h"
 #include "native_client/src/trusted/service_runtime/nacl_all_modules.h"
+#include "native_client/src/trusted/service_runtime/nacl_debug_init.h"
 #include "native_client/src/trusted/service_runtime/nacl_error_log_hook.h"
 #include "native_client/src/trusted/service_runtime/nacl_globals.h"
 #include "native_client/src/trusted/service_runtime/nacl_signal.h"
@@ -193,6 +194,7 @@ int main(int  argc,
   int                           debug_mode_ignore_validator = 0;
   int                           skip_qualification = 0;
   int                           handle_signals = 0;
+  int                           enable_debug_stub = 0;
   int                           enable_exception_handling = 0;
   struct NaClPerfCounter        time_all_main;
   const char                    **envp;
@@ -300,7 +302,7 @@ int main(int  argc,
 
       case 'g':
         handle_signals = 1;
-        nap->enable_debug_stub = 1;
+        enable_debug_stub = 1;
         break;
 
       case 'h':
@@ -814,6 +816,11 @@ int main(int  argc,
   if (!NaClAppLaunchServiceThreads(nap)) {
     fprintf(stderr, "Launch service threads failed\n");
     goto done;
+  }
+  if (enable_debug_stub) {
+    if (!NaClDebugInit(nap)) {
+      goto done;
+    }
   }
   if (!NaClCreateMainThread(nap,
                             argc - optind,

commit 1c6b8454ce2f828a04b97b03fe5e77803d096be4

Debug stub: Fix mistake in initialisation introduced in r8988
r8988 broke Chromium integration by using "args" in sel_main_chrome.c
after it had been freed and set to NULL.

Fix this by re-ordering the code.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2758
TEST=none

Review URL: https://chromiumcodereview.appspot.com/10600006

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8997 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/sel_main_chrome.c
@@ -292,9 +292,6 @@ void NaClChromeMainStart(struct NaClChromeMainArgs *args) {
   /* Load the integrated runtime (IRT) library. */
   NaClLoadIrt(nap, args->irt_fd);
 
-  free(args);
-  args = NULL;
-
   NaClEnvCleanserCtor(&env_cleanser, 1);
   if (!NaClEnvCleanserInit(&env_cleanser, envp, NULL)) {
     NaClLog(LOG_FATAL, "Failed to initialise env cleanser\n");
@@ -309,6 +306,10 @@ void NaClChromeMainStart(struct NaClChromeMainArgs *args) {
       goto done;
     }
   }
+
+  free(args);
+  args = NULL;
+
   if (!NaClCreateMainThread(nap, ac, av,
                             NaClEnvCleanserEnvironment(&env_cleanser))) {
     fprintf(stderr, "creating main thread failed\n");