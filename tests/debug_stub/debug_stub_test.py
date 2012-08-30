sbc100/native_client

commit f542269b32172cb61806f3bb0e03d0f742aea9cb

Debug stub: Switch to a thread-suspension-based model for catching faults
This changes the model for how the debug stub catches faults in
untrusted code.

The old model is that the debug stub's fault handler runs on the
thread that faulted, and this handler notifies the debug stub's main
event loop.

The new model is that service_runtime maintains a set of faulted
threads.  A thread that faults in untrusted code gets suspended,
allowing the debug stub to query and modify its register state before
resuming it.  The debug stub does this from its own thread.

 * Linux: We change the signal handler to mark a thread as faulted and
   keep it suspended (by handling SIGUSR1) until the debug stub
   unblocks it.

 * Windows: We change the debug exception handler to mark a thread as
   faulted and suspend it.

    * Change sel_main.c and sel_main_chrome.c to enable using the
      debug exception handler when using the debug stub.
    * Since the debug stub no longer relies on Windows Vectored
      Exception Handling (VEH), we can unconditionally enable our
      NTDLL patch, which makes the debug stub safer.

We remove a bunch of debug stub code for the old model:

 * Fault handling code paths (Linux and Windows):
   NaClExceptionCatcher(), SetExceptionCatch()
 * target.cc: sig_start_ and sig_done_ events, and the Signal() method.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2948
TEST=run_debug_stub_test + GDB tests

Review URL: https://chromiumcodereview.appspot.com/10836171

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9466 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/gdb_rsp/target.cc
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/platform/nacl_log.h"
 
 #include "native_client/src/trusted/gdb_rsp/abi.h"
@@ -18,6 +19,10 @@
 
 #include "native_client/src/trusted/port/platform.h"
 #include "native_client/src/trusted/port/thread.h"
+
+#include "native_client/src/trusted/service_runtime/nacl_app_thread.h"
+#include "native_client/src/trusted/service_runtime/sel_ldr.h"
+#include "native_client/src/trusted/service_runtime/thread_suspension.h"
 
 #ifdef WIN32
 #define snprintf sprintf_s
@@ -34,11 +39,10 @@ using port::MutexLock;
 namespace gdb_rsp {
 
 
-Target::Target(const Abi* abi)
-  : abi_(abi),
+Target::Target(struct NaClApp *nap, const Abi* abi)
+  : nap_(nap),
+    abi_(abi),
     mutex_(NULL),
-    sig_start_(NULL),
-    sig_done_(NULL),
     session_(NULL),
     ctx_(NULL),
     cur_signal_(0),
@@ -65,25 +69,17 @@ bool Target::Init() {
     "PacketSize=7cf;qXfer:features:read+";
 
   mutex_ = IMutex::Allocate();
-  sig_start_ = IEvent::Allocate();
-  sig_done_ = IEvent::Allocate();
   ctx_ = new uint8_t[abi_->GetContextSize()];
 
-  if ((NULL == mutex_) || (NULL == sig_start_) || (NULL == sig_done_)
-      || (NULL == ctx_)) {
+  if ((NULL == mutex_) || (NULL == ctx_)) {
     Destroy();
     return false;
   }
-
-  // Allow one exception to happen
-  sig_start_->Signal();
   return true;
 }
 
 void Target::Destroy() {
   if (mutex_) IMutex::Free(mutex_);
-  if (sig_start_) IEvent::Free(sig_start_);
-  if (sig_done_) IEvent::Free(sig_done_);
 
   delete[] ctx_;
 }
@@ -177,32 +173,17 @@ bool Target::RemoveTemporaryBreakpoints(IThread *thread) {
 }
 
 
-
-void Target::Signal(uint32_t id, int8_t sig, bool wait) {
-  // Wait for this signal's turn in the signal Q.
-  sig_start_->Wait();
-  {
-    // Now lock the target, sleeping all active threads
-    MutexLock lock(mutex_);
-
-    // Signal the stub (Run thread) that we are ready to process
-    // a trap, by updating the signal information and releasing
-    // the lock.
-    cur_signal_ = sig;
-    sig_thread_ = id;
-    reg_thread_ = id;
-  }
-
-  // Wait for permission to continue
-  if (wait) sig_done_->Wait();
-}
-
 void Target::Run(Session *ses) {
   bool first = true;
   mutex_->Lock();
   session_ = ses;
   mutex_->Unlock();
   do {
+    // We poll periodically for faulted threads or input on the socket.
+    // TODO(mseaborn): This is slow.  We should use proper thread
+    // wakeups instead.
+    // See http://code.google.com/p/nativeclient/issues/detail?id=2952
+
     // Give everyone else a chance to use the lock
     IPlatform::Relinquish(100);
 
@@ -213,50 +194,56 @@ void Target::Run(Session *ses) {
 
     uint32_t id = 0;
 
-    // If no signal is waiting for this iteration...
-    if (0 == cur_signal_) {
-      // but the debugger is talking to us then force a break
-      if (ses->DataAvailable()) {
-        // GDB should have tried to interrupt the target.
-        // See http://sourceware.org/gdb/current/onlinedocs/gdb/Interrupts.html
-        // TODO(eaeltsin): should we verify the interrupt sequence?
-
-        // Indicate we have no current thread.
-        // TODO(eaeltsin): or pick any thread? Add a test.
-        // See http://code.google.com/p/nativeclient/issues/detail?id=2743
-        sig_thread_ = 0;
-
-        // Suspend all threads.
-        if (step_over_breakpoint_thread_ == 0) {
-          IThread::SuspendAllThreadsExceptSignaled(0);
-        } else {
-          NaClLog(LOG_FATAL, "Target::Run: step over breakpoint error\n");
-        }
-      } else {
-        // otherwise, nothing to do so try again.
+    if (step_over_breakpoint_thread_ != 0) {
+      // We are waiting for a specific thread to fault while all other
+      // threads are suspended.  Note that faulted_thread_count might
+      // be >1, because multiple threads can fault simultaneously
+      // before the debug stub gets a chance to suspend all threads.
+      // This is why we must check the status of a specific thread --
+      // we cannot call UnqueueAnyFaultedThread() and expect it to
+      // return step_over_breakpoint_thread_.
+      if (!IThread::HasThreadFaulted(step_over_breakpoint_thread_)) {
+        // The thread has not faulted.  Nothing to do, so try again.
+        // Note that we do not respond to input from GDB while in this
+        // state.
+        // TODO(mseaborn): We should allow GDB to interrupt execution.
         continue;
       }
-    } else {
-      // otherwise there really is an exception so get the id of the thread
-      id = sig_thread_;
-
-      // Suspend all threads except signaled.
-      // Signaled thread is effectively suspended already, being waiting in
-      // a signal handler.
-      if (step_over_breakpoint_thread_ == 0) {
-        IThread::SuspendAllThreadsExceptSignaled(id);
-      } else if (step_over_breakpoint_thread_ != id) {
-        NaClLog(LOG_FATAL, "Target::Run: bad step_over_breakpoint_thread_\n");
-      }
-
-      // Finish step over breakpoint.
+      // All threads but one are already suspended.  We only need to
+      // suspend the single thread that we allowed to run.
+      IThread::SuspendSingleThread(step_over_breakpoint_thread_);
+      IThread::UnqueueSpecificFaultedThread(step_over_breakpoint_thread_,
+                                            &cur_signal_);
+      id = step_over_breakpoint_thread_;
       step_over_breakpoint_thread_ = 0;
-
+      sig_thread_ = id;
+      reg_thread_ = id;
       // Reset single stepping.
-      IThread *thread = threads_[id];
-      thread->SetStep(false);
+      threads_[id]->SetStep(false);
+    } else if (nap_->faulted_thread_count != 0) {
+      // At least one untrusted thread has got an exception.  First we
+      // need to ensure that all threads are suspended.  Then we can
+      // retrieve a thread from the set of faulted threads.
+      IThread::SuspendAllThreads();
+      IThread::UnqueueAnyFaultedThread(&id, &cur_signal_);
+      sig_thread_ = id;
+      reg_thread_ = id;
+      RemoveTemporaryBreakpoints(threads_[id]);
+    } else {
+      // Otherwise look for messages from GDB.
+      if (!ses->DataAvailable()) {
+        // No input from GDB.  Nothing to do, so try again.
+        continue;
+      }
+      // GDB should have tried to interrupt the target.
+      // See http://sourceware.org/gdb/current/onlinedocs/gdb/Interrupts.html
+      // TODO(eaeltsin): should we verify the interrupt sequence?
 
-      RemoveTemporaryBreakpoints(thread);
+      // Indicate we have no current thread.
+      // TODO(eaeltsin): or pick any thread? Add a test.
+      // See http://code.google.com/p/nativeclient/issues/detail?id=2743
+      sig_thread_ = 0;
+      IThread::SuspendAllThreads();
     }
 
     // Next update the current thread info
@@ -293,20 +280,13 @@ void Target::Run(Session *ses) {
     // Reset the signal value
     cur_signal_ = 0;
 
-    // Resume all threads except signaled.
-    // Signaled thread will be resumed by waking a a signal handler.
     // TODO(eaeltsin): it might make sense to resume signaled thread before
     // others, though it is not required by GDB docs.
     if (step_over_breakpoint_thread_ == 0) {
-      IThread::ResumeAllThreadsExceptSignaled(id);
-    }
-
-    // If there is no signaled thread, then we were interrupted by GDB, and
-    // there is no signal handler waiting.
-    // If there was a signaled thread, let the signal handler resume.
-    if (id != 0) {
-      sig_done_->Signal();
-      sig_start_->Signal();
+      IThread::ResumeAllThreads();
+    } else {
+      // Resume one thread while leaving all others suspended.
+      IThread::ResumeSingleThread(step_over_breakpoint_thread_);
     }
 
     // Continue running until the connection is lost.

src/trusted/debug_stub/nacl_debug.cc
@@ -28,6 +28,10 @@
 #include "native_client/src/trusted/service_runtime/nacl_debug_init.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 
+#if NACL_WINDOWS
+# include "native_client/src/trusted/service_runtime/win/debug_exception_handler.h"
+#endif
+
 using port::IPlatform;
 using port::IThread;
 using port::ITransport;
@@ -96,14 +100,6 @@ void WINAPI NaClStubThread(void *ptr) {
   }
 }
 
-void NaClExceptionCatcher(uint32_t id, int8_t sig, void *cookie) {
-  Target* targ = static_cast<Target*>(cookie);
-
-  /* Signal the target that we caught something */
-  NaClLog(LOG_WARNING, "Caught signal %d on thread %Xh.\n", sig, id);
-  targ->Signal(id, sig, true);
-}
-
 void NaClDebugThreadPrepDebugging(struct NaClAppThread *natp) throw() {
   UNREFERENCED_PARAMETER(natp);
 
@@ -142,10 +138,11 @@ void NaClDebugStop(int ErrCode) throw() {
  */
 int NaClDebugInit(struct NaClApp *nap) {
   nap->debug_stub_callbacks = &debug_callbacks;
+  nap->enable_faulted_thread_queue = 1;
   NaClDebugStubInit();
 
   CHECK(g_target == NULL);
-  g_target = new Target();
+  g_target = new Target(nap);
   CHECK(g_target != NULL);
   g_target->Init();
 
@@ -158,8 +155,15 @@ int NaClDebugInit(struct NaClApp *nap) {
   }
   g_target->AddTemporaryBreakpoint(nap->initial_entry_pt + nap->mem_start);
 
+#if NACL_WINDOWS
+  if (!NaClDebugExceptionHandlerEnsureAttached(nap)) {
+    NaClLog(LOG_ERROR, "NaClDebugInit: "
+            "Failed to attach debug exception handler\n");
+    return 0;
+  }
+#endif
+
   NaClLog(LOG_WARNING, "nacl_debug(%d) : Debugging started.\n", __LINE__);
-  IThread::SetExceptionCatch(NaClExceptionCatcher, g_target);
   CHECK(NaClThreadCtor(thread, NaClStubThread, g_target,
                        NACL_KERN_STACK_SIZE));
 

src/trusted/debug_stub/posix/thread_impl.cc
@@ -4,10 +4,7 @@
  * found in the LICENSE file.
  */
 
-#include "native_client/src/include/atomic_ops.h"
-#include "native_client/src/trusted/port/platform.h"
 #include "native_client/src/trusted/port/thread.h"
-#include "native_client/src/trusted/service_runtime/nacl_signal.h"
 
 /*
  * Define the OS specific portions of IThread interface.
@@ -15,49 +12,9 @@
 
 namespace port {
 
-static IThread::CatchFunc_t s_CatchFunc = NULL;
-static void* s_CatchCookie = NULL;
-
-
-static enum NaClSignalResult SignalHandler(int signal, void *ucontext) {
-  struct NaClSignalContext context;
-  NaClSignalContextFromHandler(&context, ucontext);
-  if (NaClSignalContextIsUntrusted(&context)) {
-    // Ensure signals are processed one at a time.
-    // Spin if we are processing another signal already. This won't actually
-    // spin for too long as that signal handler will suspend this thread.
-    // TODO(eaeltsin): this is here because Target::Signal fails doing the same
-    // using Target::sig_start_ IEvent. Investigate and remove this hack!
-    static Atomic32 g_is_processing_signal = 0;
-    while (CompareAndSwap(&g_is_processing_signal, 0, 1) != 0) {
-      // Spin.
-    }
-    uint32_t thread_id = IPlatform::GetCurrentThread();
-    IThread* thread = IThread::Acquire(thread_id);
-
-    NaClSignalContextFromHandler(thread->GetContext(), ucontext);
-    if (s_CatchFunc != NULL)
-      s_CatchFunc(thread_id, signal, s_CatchCookie);
-    NaClSignalContextToHandler(ucontext, thread->GetContext());
-
-    IThread::Release(thread);
-    // TODO(eaeltsin): remove this hack!
-    while (CompareAndSwap(&g_is_processing_signal, 1, 0) != 1) {
-      // Spin.
-    }
-    return NACL_SIGNAL_RETURN;
-  } else {
-    // Do not attempt to debug crashes in trusted code.
-    return NACL_SIGNAL_SEARCH;
-  }
+int IThread::ExceptionToSignal(int exception_code) {
+  return exception_code;
 }
-
-void IThread::SetExceptionCatch(IThread::CatchFunc_t func, void *cookie) {
-  NaClSignalHandlerAdd(SignalHandler);
-  s_CatchFunc = func;
-  s_CatchCookie = cookie;
-}
-
 
 }  // End of port namespace
 

src/trusted/debug_stub/thread_common.cc
@@ -141,26 +141,22 @@ void IThread::Release(IThread *ithread) {
   }
 }
 
-void IThread::SuspendAllThreadsExceptSignaled(uint32_t signaled_tid) {
+void IThread::SuspendAllThreads() {
   MutexLock lock(ThreadGetLock());
   ThreadMap_t &map = *ThreadGetMap();
 
   CHECK(!map.empty());
 
-  NaClUntrustedThreadsSuspendAllButOne(
-      map.begin()->second->natp_->nap,
-      signaled_tid == 0 ? NULL : map[signaled_tid]->natp_,
-      /* save_registers= */ 1);
+  NaClUntrustedThreadsSuspendAll(map.begin()->second->natp_->nap,
+                                 /* save_registers= */ 1);
 
   for (ThreadMap_t::iterator it = map.begin(); it != map.end(); ++it) {
     Thread *thread = it->second;
-    if (thread->id_ != signaled_tid) {
-      NaClAppThreadGetSuspendedRegisters(thread->natp_, &thread->context_);
-    }
+    NaClAppThreadGetSuspendedRegisters(thread->natp_, &thread->context_);
   }
 }
 
-void IThread::ResumeAllThreadsExceptSignaled(uint32_t signaled_tid) {
+void IThread::ResumeAllThreads() {
   MutexLock lock(ThreadGetLock());
   ThreadMap_t &map = *ThreadGetMap();
 
@@ -168,14 +164,65 @@ void IThread::ResumeAllThreadsExceptSignaled(uint32_t signaled_tid) {
 
   for (ThreadMap_t::iterator it = map.begin(); it != map.end(); ++it) {
     Thread *thread = it->second;
-    if (thread->id_ != signaled_tid) {
-      NaClAppThreadSetSuspendedRegisters(thread->natp_, &thread->context_);
-    }
+    NaClAppThreadSetSuspendedRegisters(thread->natp_, &thread->context_);
   }
 
-  NaClUntrustedThreadsResumeAllButOne(
-      map.begin()->second->natp_->nap,
-      signaled_tid == 0 ? NULL : map[signaled_tid]->natp_);
+  NaClUntrustedThreadsResumeAll(map.begin()->second->natp_->nap);
+}
+
+void IThread::SuspendSingleThread(uint32_t thread_id) {
+  MutexLock lock(ThreadGetLock());
+  Thread *thread = (*ThreadGetMap())[thread_id];
+  NaClUntrustedThreadSuspend(thread->natp_, /* save_registers= */ 1);
+  NaClAppThreadGetSuspendedRegisters(thread->natp_, &thread->context_);
+}
+
+void IThread::ResumeSingleThread(uint32_t thread_id) {
+  MutexLock lock(ThreadGetLock());
+  Thread *thread = (*ThreadGetMap())[thread_id];
+  NaClAppThreadSetSuspendedRegisters(thread->natp_, &thread->context_);
+  NaClUntrustedThreadResume(thread->natp_);
+}
+
+// HasThreadFaulted() returns whether the given thread has been
+// blocked as a result of faulting.  The thread does not need to be
+// currently suspended.
+bool IThread::HasThreadFaulted(uint32_t thread_id) {
+  MutexLock lock(ThreadGetLock());
+  Thread *thread = (*ThreadGetMap())[thread_id];
+  return thread->natp_->fault_signal != 0;
+}
+
+// UnqueueSpecificFaultedThread() takes a thread that has been blocked
+// as a result of faulting and unblocks it, returning the type of
+// fault via |signal|.  As a precondition, the thread must be
+// currently suspended.
+void IThread::UnqueueSpecificFaultedThread(uint32_t thread_id,
+                                           int8_t *signal) {
+  MutexLock lock(ThreadGetLock());
+  Thread *thread = (*ThreadGetMap())[thread_id];
+  int exception_code;
+  CHECK(NaClAppThreadUnblockIfFaulted(thread->natp_, &exception_code));
+  *signal = IThread::ExceptionToSignal(exception_code);
+}
+
+// UnqueueAnyFaultedThread() picks a thread that has been blocked as a
+// result of faulting and unblocks it.  It returns the thread's ID via
+// |thread_id| and the type of fault via |signal|.  As a precondition,
+// all threads must be currently suspended.
+void IThread::UnqueueAnyFaultedThread(uint32_t *thread_id, int8_t *signal) {
+  MutexLock lock(ThreadGetLock());
+  ThreadMap_t *map = ThreadGetMap();
+  for (ThreadMap_t::iterator it = map->begin(); it != map->end(); ++it) {
+    Thread *thread = it->second;
+    int exception_code;
+    if (NaClAppThreadUnblockIfFaulted(thread->natp_, &exception_code)) {
+      *signal = IThread::ExceptionToSignal(exception_code);
+      *thread_id = thread->GetId();
+      return;
+    }
+  }
+  NaClLog(LOG_FATAL, "UnqueueAnyFaultedThread: No threads queued\n");
 }
 
 }  // namespace port

src/trusted/debug_stub/win/thread_impl.cc
@@ -6,26 +6,14 @@
 
 #include <windows.h>
 
-#include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/trusted/port/thread.h"
-#include "native_client/src/trusted/service_runtime/nacl_app_thread.h"
 #include "native_client/src/trusted/service_runtime/nacl_signal.h"
 
 /*
  * Define the OS specific portions of IThread interface.
  */
 
-namespace {
-
-const int kDBG_PRINTEXCEPTION_C = 0x40010006;
-
-}  // namespace
-
 namespace port {
-
-static IThread::CatchFunc_t s_CatchFunc = NULL;
-static void* s_CatchCookie = NULL;
-static PVOID s_OldCatch = NULL;
 
 enum PosixSignals {
   SIGINT  = 2,
@@ -39,7 +27,7 @@ enum PosixSignals {
   SIGSTKFLT = 16,
 };
 
-static int8_t ExceptionToSignal(int ex) {
+int IThread::ExceptionToSignal(int ex) {
   switch (ex) {
     case EXCEPTION_GUARD_PAGE:
     case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
@@ -79,77 +67,6 @@ static int8_t ExceptionToSignal(int ex) {
       return SIGILL;
   }
   return SIGILL;
-}
-
-static LONG NTAPI ExceptionCatch(PEXCEPTION_POINTERS ep) {
-  struct NaClSignalContext context;
-
-  // This 2 lines is a fix for the bug:
-  // 366: Linux GDB doesn't work for Chrome
-  // http://code.google.com/p/nativeclient/issues/detail?id=366
-  // When debug stub thread opens socket to listen (for RSP debugger),
-  // it triggers some component to send DBG_PRINTEXCEPTION(with string
-  // "swi_lsp: non-browser app; disable"), then VEH handler goes into wait
-  // for debugger to resolve exception.
-  // But debugger is not connected, and debug thread is not listening on
-  // connection! It get stuck.
-  // Ignoring this exception - for now - helps debug stub start on chrome.
-  // Now it can listen on RSP connection and can get debugger connected etc.
-  if (kDBG_PRINTEXCEPTION_C == ep->ExceptionRecord->ExceptionCode) {
-    return EXCEPTION_CONTINUE_EXECUTION;
-  }
-
-  NaClSignalContextFromHandler(&context, ep->ContextRecord);
-  if (NaClSignalContextIsUntrusted(&context)) {
-    // Ensure signals are processed one at a time.
-    // Spin if we are processing another signal already. This won't actually
-    // spin for too long as that signal handler will suspend this thread.
-    // TODO(eaeltsin): this is here because Target::Signal fails doing the same
-    // using Target::sig_start_ IEvent. Investigate and remove this hack!
-    static Atomic32 g_is_processing_signal = 0;
-    while (CompareAndSwap(&g_is_processing_signal, 0, 1) != 0) {
-      // Spin.
-    }
-
-    uint32_t id = static_cast<uint32_t>(GetCurrentThreadId());
-    IThread* thread = IThread::Acquire(id);
-    *thread->GetContext() = context;
-    // Handle EXCEPTION_BREAKPOINT SEH/VEH handler special case:
-    // Here instruction pointer from the CONTEXT structure points to the int3
-    // instruction, not after the int3 instruction.
-    // This is different from the hardware context, and (thus) different from
-    // the context obtained via GetThreadContext on Windows and from signal
-    // handler context on Linux.
-    // See http://code.google.com/p/nativeclient/issues/detail?id=1730.
-    // We adjust instruction pointer to match the hardware.
-    if (ep->ExceptionRecord->ExceptionCode == EXCEPTION_BREAKPOINT) {
-      thread->GetContext()->prog_ctr += 1;
-    }
-
-    int8_t sig = ExceptionToSignal(ep->ExceptionRecord->ExceptionCode);
-    if (NULL != s_CatchFunc) s_CatchFunc(id, sig, s_CatchCookie);
-    NaClSignalContextToHandler(ep->ContextRecord, thread->GetContext());
-
-    IThread::Release(thread);
-    // TODO(eaeltsin): remove this hack!
-    while (CompareAndSwap(&g_is_processing_signal, 1, 0) != 1) {
-      // Spin.
-    }
-    return EXCEPTION_CONTINUE_EXECUTION;
-  } else {
-    // Do not attempt to debug crashes in trusted code.
-    return EXCEPTION_CONTINUE_SEARCH;
-  }
-}
-
-void IThread::SetExceptionCatch(IThread::CatchFunc_t func, void *cookie) {
-  // Remove our old catch if there is one, this allows us to add again
-  if (NULL != s_OldCatch) RemoveVectoredExceptionHandler(s_OldCatch);
-
-  // Add the new one, at the front of the list
-  s_OldCatch = AddVectoredExceptionHandler(1, ExceptionCatch);
-  s_CatchFunc = func;
-  s_CatchCookie = cookie;
 }
 
 }  // namespace port

src/trusted/gdb_rsp/target.h
@@ -33,6 +33,8 @@
 #include "native_client/src/trusted/port/mutex.h"
 #include "native_client/src/trusted/port/thread.h"
 
+struct NaClApp;
+
 namespace gdb_rsp {
 
 class Abi;
@@ -61,7 +63,7 @@ class Target {
 
  public:
   // Contruct a Target object.  By default use the native ABI.
-  explicit Target(const Abi *abi = NULL);
+  explicit Target(struct NaClApp *nap, const Abi *abi = NULL);
   ~Target();
 
   // Init must be the first function called to correctlty
@@ -76,12 +78,6 @@ class Target {
   // seeing the modified memory.
   bool AddTemporaryBreakpoint(uint64_t address);
   bool RemoveTemporaryBreakpoints(port::IThread *thread);
-
-  // This function should be called by a tracked thread when it takes
-  // an exception.  It takes sig_start_ to prevent other exceptions
-  // from signalling thread.  If wait is true, it will then  block on
-  // sig_done_ until a continue is issued by the host.
-  void Signal(uint32_t id, int8_t sig, bool wait);
 
   // This function will spin on a session, until it closes.  If an
   // exception is caught, it will signal the exception thread by
@@ -123,17 +119,11 @@ class Target {
   port::IThread *GetThread(uint32_t id);
 
  private:
+  struct NaClApp *nap_;
   const Abi *abi_;
 
   // This mutex protects debugging state (threads_, cur_signal, sig_thread_)
   port::IMutex *mutex_;
-
-  // This event is signalled when the target is really to process an
-  // exception.  It ensures only one exception is processed at a time.
-  port::IEvent *sig_start_;
-
-  // This event is signalled when the target done processing an exception.
-  port::IEvent *sig_done_;
 
   Session *session_;
 
@@ -148,11 +138,11 @@ class Target {
 
   // Signal being processed.
   // Set to 0 when execution was interrupted by GDB and not by a signal.
-  volatile int8_t cur_signal_;
+  int8_t cur_signal_;
 
   // Signaled thread id.
   // Set to 0 when execution was interrupted by GDB and not by a signal.
-  volatile uint32_t sig_thread_;
+  uint32_t sig_thread_;
 
   // Thread for subsequent registers access operations.
   uint32_t reg_thread_;
@@ -161,6 +151,7 @@ class Target {
   // suspended.
   uint32_t step_over_breakpoint_thread_;
 
+  // TODO(mseaborn): Remove this, because we can get mem_start from nap_.
   uint64_t mem_base_;
 };
 

src/trusted/port/thread.h
@@ -40,9 +40,14 @@ class IThread {
   static IThread *Create(uint32_t id, struct NaClAppThread *natp);
   static IThread *Acquire(uint32_t id);
   static void Release(IThread *thread);
-  static void SuspendAllThreadsExceptSignaled(uint32_t signaled_tid);
-  static void ResumeAllThreadsExceptSignaled(uint32_t signaled_tid);
-  static void SetExceptionCatch(CatchFunc_t func, void *cookie);
+  static void SuspendAllThreads();
+  static void ResumeAllThreads();
+  static void SuspendSingleThread(uint32_t thread_id);
+  static void ResumeSingleThread(uint32_t thread_id);
+  static bool HasThreadFaulted(uint32_t thread_id);
+  static void UnqueueSpecificFaultedThread(uint32_t thread_id, int8_t *signal);
+  static void UnqueueAnyFaultedThread(uint32_t *thread_id, int8_t *signal);
+  static int ExceptionToSignal(int exception_code);
 
  protected:
   virtual ~IThread() {}  // Prevent delete of base pointer

commit 043a3f7052dcb6a5bf584fc7d9af84c539c70b93

Debug stub: Ensure that continuing works after single-stepping
Ensure that the x86 trap flag is always reset to zero after receiving
a fault.

This fixes a regression introduced by r9466.

Also tidy up to remove the "id" variable: use sig_thread_ instead,
since that contains the same value.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=3006
TEST=run_debug_stub_test

Review URL: https://chromiumcodereview.appspot.com/10898041

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9604 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/debug_stub/target.cc
@@ -166,8 +166,6 @@ void Target::Run(Session *ses) {
     MutexLock lock(mutex_);
     Packet recv, reply;
 
-    uint32_t id = 0;
-
     if (step_over_breakpoint_thread_ != 0) {
       // We are waiting for a specific thread to fault while all other
       // threads are suspended.  Note that faulted_thread_count might
@@ -188,21 +186,17 @@ void Target::Run(Session *ses) {
       IThread::SuspendSingleThread(step_over_breakpoint_thread_);
       IThread::UnqueueSpecificFaultedThread(step_over_breakpoint_thread_,
                                             &cur_signal_);
-      id = step_over_breakpoint_thread_;
+      sig_thread_ = step_over_breakpoint_thread_;
+      reg_thread_ = step_over_breakpoint_thread_;
       step_over_breakpoint_thread_ = 0;
-      sig_thread_ = id;
-      reg_thread_ = id;
-      // Reset single stepping.
-      threads_[id]->SetStep(false);
     } else if (nap_->faulted_thread_count != 0) {
       // At least one untrusted thread has got an exception.  First we
       // need to ensure that all threads are suspended.  Then we can
       // retrieve a thread from the set of faulted threads.
       IThread::SuspendAllThreads();
-      IThread::UnqueueAnyFaultedThread(&id, &cur_signal_);
-      sig_thread_ = id;
-      reg_thread_ = id;
-      RemoveTemporaryBreakpoints(threads_[id]);
+      IThread::UnqueueAnyFaultedThread(&sig_thread_, &cur_signal_);
+      reg_thread_ = sig_thread_;
+      RemoveTemporaryBreakpoints(threads_[sig_thread_]);
     } else {
       // Otherwise look for messages from GDB.
       if (!ses->DataAvailable()) {
@@ -220,9 +214,14 @@ void Target::Run(Session *ses) {
       IThread::SuspendAllThreads();
     }
 
+    if (sig_thread_ != 0) {
+      // Reset single stepping.
+      threads_[sig_thread_]->SetStep(false);
+    }
+
     // Next update the current thread info
     char tmp[16];
-    snprintf(tmp, sizeof(tmp), "QC%x", id);
+    snprintf(tmp, sizeof(tmp), "QC%x", sig_thread_);
     properties_["C"] = tmp;
 
     if (first) {

tests/debug_stub/debug_stub_test.py
@@ -108,6 +108,9 @@ IP_REG = {
     }
 
 
+X86_TRAP_FLAG = 1 << 8
+
+
 def DecodeRegs(reply):
   defs = REG_DEFS[ARCH]
   names = [reg_name for reg_name, reg_fmt in defs]
@@ -396,8 +399,10 @@ class DebugStubTest(unittest.TestCase):
       reply = connection.RspRequest(step_command)
       self.assertEqual(reply, stop_reply)
       ip += size
-      actual_ip = DecodeRegs(connection.RspRequest('g'))[IP_REG[ARCH]]
-      self.assertEqual(actual_ip, ip)
+      regs = DecodeRegs(connection.RspRequest('g'))
+      self.assertEqual(regs[IP_REG[ARCH]], ip)
+      # The trap flag should be reported as unset.
+      self.assertEqual(regs['eflags'] & X86_TRAP_FLAG, 0)
 
   def test_single_step(self):
     if ARCH == 'arm':
@@ -411,6 +416,9 @@ class DebugStubTest(unittest.TestCase):
       AssertReplySignal(reply, NACL_SIGTRAP)
 
       self.CheckSingleStep(connection, 's', reply)
+      # Check that we can continue after single-stepping.
+      reply = connection.RspRequest('c')
+      self.assertEquals(reply, 'W00')
 
   def test_vCont(self):
     # Basically repeat test_single_step, but using vCont commands.