sbc100/native_client

commit 00d0d2c0499abc1049d982b5ebac0e19e4bbd920

toolchain_build_pnacl.py: Build newlib
Build newlib, the first of the PNaCl target bitcode libraries.
Because the toolchain components are not assembled together incrementally as
as they were with build.sh, use a copy of the driver scripts with a driver.conf
that points to the host binaries to build the target libs.

R=jvoung@chromium.org
BUG= https://code.google.com/p/nativeclient/issues/detail?id=3719

Review URL: https://codereview.chromium.org/138063010

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12628 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

toolchain_build/toolchain_build.py
@@ -372,7 +372,7 @@ def WithDepsOptions(options, component=None):
 # to Google Storage are constrained.  GNU configuration tuples contain
 # dashes, which we translate to underscores.
 def ForHost(component_name, host):
-  return component_name + '_' + re.sub(r'[^A-Za-z0-9_/.]', '_', host)
+  return component_name + '_' + gsd_storage.LegalizeName(host)
 
 
 # These are libraries that go into building the compiler itself.

build/gsd_storage.py
@@ -14,6 +14,7 @@ more or less like a key+value data-store.
 
 import logging
 import os
+import re
 import shutil
 import subprocess
 import sys
@@ -26,6 +27,14 @@ import http_download
 GS_PATTERN = 'gs://%s'
 GS_HTTPS_PATTERN = 'https://storage.googleapis.com/%s'
 
+
+def LegalizeName(name):
+  """ Return a file name suitable for uploading to Google Storage.
+
+  The names of such files cannot contain dashes or other non-identifier
+  characters.
+  """
+  return re.sub(r'[^A-Za-z0-9_/.]', '_', name)
 
 def HttpDownload(url, target):
   """Default download route."""

toolchain_build/pnacl_commands.py
@@ -30,7 +30,7 @@ DRIVER_UTILS = [name + '.py' for name in
                      'shelltools')]
 
 def InstallDriverScripts(subst, srcdir, dstdir, host_windows=False,
-                         host_64bit=False):
+                         host_64bit=False, extra_config=[]):
   srcdir = subst.SubstituteAbsPaths(srcdir)
   dstdir = subst.SubstituteAbsPaths(dstdir)
   file_tools.MakeDirectoryIfAbsent(os.path.join(dstdir, 'pydir'))
@@ -53,3 +53,5 @@ def InstallDriverScripts(subst, srcdir, dstdir, host_windows=False,
   with open(os.path.join(dstdir, 'driver.conf'), 'w') as f:
     print >> f, 'HAS_FRONTEND=1'
     print >> f, 'HOST_ARCH=x86_64' if host_64bit else 'HOST_ARCH=x86_32'
+    for line in extra_config:
+      print >> f, subst.Substitute(line)

toolchain_build/pnacl_targetlibs.py
@@ -9,10 +9,74 @@
 import toolchain_env
 
 import os
+import re
 import sys
 
 import command
+import fnmatch
+import gsd_storage
+import platform_tools
 import pnacl_commands
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+NACL_DIR = os.path.dirname(SCRIPT_DIR)
+
+# Return the path to the local copy of the driver script.
+# msys should be false if the path will be called directly rather than passed to
+# an msys or cygwin tool such as sh or make.
+def PnaclTool(toolname, msys=True):
+  ext = '.bat' if not msys and platform_tools.IsWindows() else ''
+  return command.path.join('%(cwd)s', 'driver', 'pnacl-' + toolname + ext)
+
+# PNaCl tools for newlib's environment, e.g. CC_FOR_TARGET=/path/to/pnacl-clang
+TOOL_ENV_NAMES = { 'CC': 'clang', 'CXX': 'clang++', 'AR': 'ar', 'NM': 'nm',
+                   'RANLIB': 'ranlib', 'READELF': 'readelf',
+                   'OBJDUMP': 'illegal', 'AS': 'illegal', 'LD': 'illegal',
+                   'STRIP': 'illegal' }
+TARGET_TOOLS = [ tool + '_FOR_TARGET=' + PnaclTool(name)
+                 for tool, name in TOOL_ENV_NAMES.iteritems() ]
+
+TARGET_BCLIB_CFLAGS = '-g -O2 -mllvm -inline-threshold=5'
+NEWLIB_ISYSTEM_CFLAGS = ' '.join([
+    '-isystem',
+    command.path.join('%(abs_newlib)s', 'include')])
+
+
+def MakeCommand():
+  make_command = ['make']
+  if not platform_tools.IsWindows() and not command.Runnable.use_cygwin:
+    # The make that ships with msys sometimes hangs when run with -j.
+    # The ming32-make that comes with the compiler itself reportedly doesn't
+    # have this problem, but it has issues with pathnames with LLVM's build.
+    make_command.append('-j%(cores)s')
+  return make_command
+
+# Return the component name to use for a component name with
+# a host triple. GNU configuration triples contain dashes, which are converted
+# to underscores so the names are legal for Google Storage.
+def Mangle(component_name, extra):
+  return component_name + '_' + gsd_storage.LegalizeName(extra)
+
+def TripleIsWindows(t):
+  return fnmatch.fnmatch(t, '*-mingw32*') or fnmatch.fnmatch(t, '*cygwin*')
+
+# Copy the driver scripts to the working directory, with extra config to set
+# paths to the compiled host binaries. This could also be done by injecting -B
+# flags into all the target tool command lines, but the library builds don't
+# support that for tools other than CC (i.e. there are CFLAGS but no overridable
+# flags for binutils).
+def CopyDriverForTargetLib(host):
+  return [
+      command.RemoveDirectory('driver'),
+      command.Mkdir('driver'),
+      command.Runnable(pnacl_commands.InstallDriverScripts,
+                       '%(driver)s', '%(cwd)s/driver',
+                       host_windows=TripleIsWindows(host),
+                       host_64bit=False,
+                       extra_config=['BPREFIXES=%(abs_' + Mangle('llvm', host) +
+                        ')s %(abs_' + Mangle('binutils_pnacl', host) + ')s'])
+      ]
+
 
 def TargetLibsSrc(GitSyncCmd):
   newlib_sys_nacl = command.path.join('%(output)s',
@@ -64,3 +128,57 @@ def TargetLibsSrc(GitSyncCmd):
       },
   }
   return source
+
+
+def BitcodeLibs(host):
+  def H(name):
+    return Mangle(name, host)
+  libs = {
+      'newlib': {
+          'type': 'build',
+          'dependencies': [ 'newlib_src', H('llvm'),
+                            H('binutils_pnacl'),],
+          'inputs': { 'driver': os.path.join(NACL_DIR, 'pnacl', 'driver')},
+          'commands' :
+              CopyDriverForTargetLib(host) + [
+              command.SkipForIncrementalCommand(
+                  ['sh', '%(newlib_src)s/configure'] +
+                  TARGET_TOOLS +
+                  ['CFLAGS_FOR_TARGET=' + TARGET_BCLIB_CFLAGS + ' -allow-asm',
+                  '--disable-multilib',
+                  '--prefix=',
+                  '--disable-newlib-supplied-syscalls',
+                  '--disable-texinfo',
+                  '--disable-libgloss',
+                  '--enable-newlib-iconv',
+                  '--enable-newlib-iconv-from-encodings=' +
+                  'UTF-8,UTF-16LE,UCS-4LE,UTF-16,UCS-4',
+                  '--enable-newlib-iconv-to-encodings=' +
+                  'UTF-8,UTF-16LE,UCS-4LE,UTF-16,UCS-4',
+                  '--enable-newlib-io-long-long',
+                  '--enable-newlib-io-long-double',
+                  '--enable-newlib-io-c99-formats',
+                  '--enable-newlib-mb',
+                  '--target=le32-nacl'
+              ]),
+              command.Command(MakeCommand()),
+              command.Command(['make', 'DESTDIR=%(abs_output)s', 'install']),
+              command.CopyTree(command.path.join('%(output)s', 'le32-nacl',
+                                                 'lib'),
+                               command.path.join('%(output)s', 'lib')),
+              command.CopyTree(
+                  command.path.join('%(output)s','le32-nacl', 'include'),
+                  command.path.join('%(output)s','include')),
+              command.RemoveDirectory(command.path.join('%(output)s',
+                                                        'le32-nacl')),
+              command.Mkdir(os.path.join('%(output)s', 'include', 'nacl')),
+              # Copy nacl_random.h, used by libc++. It uses the IRT, so should
+              # be safe to include in the toolchain.
+              command.Copy(os.path.join('%(top_srcdir)s', 'src', 'untrusted',
+                                        'nacl', 'nacl_random.h'),
+                           os.path.join('%(output)s', 'include', 'nacl',
+                                        'nacl_random.h')),
+          ],
+      },
+  }
+  return libs

toolchain_build/toolchain_build_pnacl.py
@@ -27,6 +27,7 @@ import sys
 
 import command
 import file_tools
+import gsd_storage
 import platform_tools
 import pnacl_commands
 import pnacl_targetlibs
@@ -221,17 +222,10 @@ def HostToolsSources(GetGitSyncCmd):
   return sources
 
 
-# Return the component name we'll use for a base component name and
-# a host triple.  The component names cannot contain dashes or other
-# non-identifier characters, because the names of the files uploaded
-# to Google Storage are constrained.  GNU configuration tuples contain
-# dashes, which we translate to underscores.
-def Mangle(component_name, extra):
-  return component_name + '_' + re.sub(r'[^A-Za-z0-9_/.]', '_', extra)
-
 def HostTools(host):
   def H(component_name):
-    return Mangle(component_name, host)
+    # Return a package name for a component name with a host triple.
+    return component_name + '_' + gsd_storage.LegalizeName(host)
   tools = {
       H('binutils_pnacl'): {
           'dependencies': ['binutils_pnacl_src'],
@@ -385,6 +379,9 @@ if __name__ == '__main__':
     hosts.append('i686-w64-mingw32')
   for host in hosts:
     packages.update(HostTools(host))
+  # On linux use the 32-bit compiler to build the target libs since that's what
+  # most developers will be using.
+  packages.update(pnacl_targetlibs.BitcodeLibs(hosts[0]))
 
   tb = toolchain_main.PackageBuilder(packages,
                                      leftover_args)

commit 3c0798a2db04871794a4f11a84b897af1e6802cc

Add missing import to toolchain_build.py
It was broken by r12628

R=mcgrathr@chromium.org
BUG=none

Review URL: https://codereview.chromium.org/141583007

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12633 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

toolchain_build/toolchain_build.py
@@ -15,6 +15,7 @@ import re
 import sys
 
 import command
+import gsd_storage
 import toolchain_main
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))